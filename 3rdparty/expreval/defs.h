1d3cfbfacf2761b79d2536cb1ede02d01ad56a32
kmo <kmo@e10b68d5-8a6e-419e-a041-bce267b0401d>
2011-12-08 13:18:01 +0000

    added: expreval third party library
    this is a library to perform analytic function evaluation
    
    git-svn-id: http://svn.sintef.no/trondheim/IFEM/trunk@1335 e10b68d5-8a6e-419e-a041-bce267b0401d

---
+++ 3rdparty/expreval/defs.h
@@ -0,0 +1,26 @@
+// File:    defs.h
+// Author:  Brian Vanderburg II
+// Purpose: Definitions for ExprEval
+//------------------------------------------------------------------------------
+
+#ifndef __EXPREVAL_DEFS_H
+#define __EXPREVAL_DEFS_H
+
+namespace ExprEval
+{
+    // constants
+    const double EXPREVAL_PI = 3.14159265358979323846;
+    const double EXPREVAL_E = 2.7182818284590452354;
+    
+} // namespace ExprEval
+
+// CHANGED from original code: due to auto_ptr deprecation in c++0x
+#ifdef __GXX_EXPERIMENTAL_CXX0X__
+#define aptr(x) std::unique_ptr<x>
+#else
+#define aptr(x) std::auto_ptr<x>
+#endif
+
+
+#endif // __EXPREVAL_DEFS_H
+
