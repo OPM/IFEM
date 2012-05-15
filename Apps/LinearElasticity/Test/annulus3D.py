9beab21e83820d8e79b9e5505a65bbd3bbfb89cb
kmo <kmo@e10b68d5-8a6e-419e-a041-bce267b0401d>
2012-05-15 11:24:21 +0000

    Added: 3D test models for Dirichlet conditions in local axes
    git-svn-id: http://svn.sintef.no/trondheim/IFEM/trunk@1696 e10b68d5-8a6e-419e-a041-bce267b0401d

--- Apps/LinearElasticity/Test/annulus3D.py
+++ Apps/LinearElasticity/Test/annulus3D.py
@@ -1,9 +1,10 @@
 from GoTools import *
 from GoTools.CurveFactory import *
 from GoTools.SurfaceFactory import *
+from GoTools.VolumeFactory import *
 from math import *
 
-SetFinalOutput("annulus.g2")
+SetFinalOutput("annulus3D.g2")
 
 r1 = 5
 r2 = 7
@@ -13,4 +14,9 @@ outer = CircleSegment([0,0,0],[-r2,0,0],pi,[0,0,1])
 
 surf = LoftCurves([inner,outer])
 
-FinalOutput(surf,True)
+line = LineSegment([0,0,0],[0,0,1])
+volum = LinearSurfaceSweep(surf,line,[0,0,0])
+
+volum.MakeRHS()
+
+FinalOutput(volum,True)
