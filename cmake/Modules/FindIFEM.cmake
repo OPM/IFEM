1409a782ec4a6a0ddad00af0af7e3aaefcc9ba46
akva <akva@e10b68d5-8a6e-419e-a041-bce267b0401d>
2012-08-16 15:26:58 +0000

    changed: restructure the elasticity apps
    shared files are now in Apps/Elasticity, those only relating to the
    linear elasticity app in Apps/Elasticity/Linear and the Finite
    Deformation code sits in Apps/Elasticity/FiniteDeformation
    
    git-svn-id: http://svn.sintef.no/trondheim/IFEM/trunk@1874 e10b68d5-8a6e-419e-a041-bce267b0401d

--- cmake/Modules/FindIFEM.cmake
+++ cmake/Modules/FindIFEM.cmake
@@ -1,42 +1,38 @@
 IF(NOT DEFINED FORCE_SYSTEM_IFEM OR NOT "${FORCE_SYSTEM_IFEM}")
-  FIND_PATH(IFEM_INCLUDES
-    NAMES ASMbase.h
-    PATHS ${PROJECT_SOURCE_DIR}/../../src/ASM)
+  FIND_PATH(IFEM_PATH
+    NAMES IFEM.h
+    PATHS ${PROJECT_SOURCE_DIR}/../../src/
+          ${PROJECT_SOURCE_DIR}/../../../src/)
 ENDIF(NOT DEFINED FORCE_SYSTEM_IFEM OR NOT "${FORCE_SYSTEM_IFEM}")
 
 FIND_PACKAGE(IFEMDeps)
 
-IF(IFEM_INCLUDES)
+IF(IFEM_PATH)
   # Build is in-tree
   MESSAGE(STATUS "Using in-tree libIFEM")
 
-  FIND_PATH(Int_INCL AnaSol.h  ${PROJECT_SOURCE_DIR}/../../src/Integrands)
-  FIND_PATH(SIM_INCL SIMbase.h ${PROJECT_SOURCE_DIR}/../../src/SIM)
-  FIND_PATH(Alg_INCL MatVec.h  ${PROJECT_SOURCE_DIR}/../../src/LinAlg)
-  FIND_PATH(Utl_INCL Vec3.h    ${PROJECT_SOURCE_DIR}/../../src/Utility)
-  SET(IFEM_INCLUDES ${IFEM_INCLUDES}
-                    ${Int_INCL}
-                    ${SIM_INCL}
-                    ${Alg_INCL}
-                    ${Utl_INCL})
+  SET(IFEM_INCLUDES ${IFEM_PATH}
+                    ${IFEM_PATH}/ASM
+                    ${IFEM_PATH}/Eig
+                    ${IFEM_PATH}/LinAlg
+                    ${IFEM_PATH}/SIM
+                    ${IFEM_PATH}/Utility)
 
   FIND_LIBRARY(IFEM_LIBRARIES
     NAMES IFEM
-    PATHS ${PROJECT_SOURCE_DIR}/../../${IFEM_BUILD_TYPE}/lib
-    PATHS ${PROJECT_SOURCE_DIR}/../../${CMAKE_BUILD_TYPE}/lib
-    ${PROJECT_SOURCE_DIR}/../../lib
+    PATHS ${IFEM_PATH}/../${IFEM_BUILD_TYPE}/lib
+          ${IFEM_PATH}/../lib
     NO_DEFAULT_PATH)
   IF(NOT IFEM_LIBRARIES)
     MESSAGE(WARNING "Could not find the in-tree libIFEM library, "
       "we assume it will be built into a build-type dir")
-    SET(IFEM_LIBRARIES ${PROJECT_SOURCE_DIR}/../../${IFEM_BUILD_TYPE}/lib/libIFEM.a)
   ENDIF(NOT IFEM_LIBRARIES)
 
   IF(NOT IFEM_USE_SYSTEM_TINYXML)
     SET(IFEM_INCLUDES ${IFEM_INCLUDES}
-                      ${PROJECT_SOURCE_DIR}/../../3rdparty/tinyxml)
+                      ${IFEM_PATH}/../3rdparty/tinyxml)
   ENDIF(NOT IFEM_USE_SYSTEM_TINYXML)
-ELSE(IFEM_INCLUDES)
+ELSE(IFEM_PATH)
   IF(NOT DEFINED FORCE_SYSTEM_IFEM OR NOT "${FORCE_SYSTEM_IFEM}")
     MESSAGE(STATUS "No in-tree libIFEM found, looking for system library")
   ENDIF(NOT DEFINED FORCE_SYSTEM_IFEM OR NOT "${FORCE_SYSTEM_IFEM}")
@@ -49,7 +45,7 @@ ELSE(IFEM_INCLUDES)
   FIND_LIBRARY(IFEM_LIBRARIES
     NAMES IFEM
     PATHS $ENV{HOME}/lib)
-ENDIF(IFEM_INCLUDES)
+ENDIF(IFEM_PATH)
 
 INCLUDE(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(IFEM DEFAULT_MSG
