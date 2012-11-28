88eec060e0479433d83b7000423189df3afa97dd
akva <akva@e10b68d5-8a6e-419e-a041-bce267b0401d>
2012-11-28 17:23:46 +0000

    added: common TimeIntegration namespace
    holds method enumeration + some helpers
    
    git-svn-id: http://svn.sintef.no/trondheim/IFEM/trunk@2170 e10b68d5-8a6e-419e-a041-bce267b0401d

---
+++ Apps/Common/TimeIntUtils.C
@@ -0,0 +1,43 @@
+//==============================================================================
+//!
+//! \file TimeIntUtils.h
+//!
+//! \date Nov 28 2012
+//!
+//! \author Arne Morten Kvarving / SINTEF
+//!
+//! \brief Various helpers for time integration
+//!
+//==============================================================================
+
+#include "TimeIntUtils.h"
+
+namespace TimeIntegration {
+
+int Order(Method method)
+{
+  if (method == EULER || method == BE)
+    return 1;
+
+  if (method == HEUN || method == BDF2)
+    return 2;
+
+  if (method == RK3)
+    return 3;
+
+  if (method == RK4)
+    return 3;
+
+  return 0;
+}
+
+
+int Steps(Method method)
+{
+  if (method == BDF2)
+    return 2;
+
+  return 1;
+}
+
+}
---
+++ Apps/Common/TimeIntUtils.h
@@ -0,0 +1,43 @@
+//==============================================================================
+//!
+//! \file TimeIntUtils.h
+//!
+//! \date Nov 28 2012
+//!
+//! \author Arne Morten Kvarving / SINTEF
+//!
+//! \brief Various helpers for time integration
+//!
+//==============================================================================
+
+#ifndef _TIME_INT_UTILS_H
+#define _TIME_INT_UTILS_H
+
+
+namespace TimeIntegration //!< Time integration scope
+{
+  //! \brief Enum defining various solution methods.
+  enum Method
+  {
+    NONE  = 0,  //!< No time integration
+    EULER = 1,  //!< Forward Euler, explicit
+    HEUN  = 2,  //!< Heun-Euler, explicit
+    RK3   = 3,  //!< Kutta's third order method, explicit
+    RK4   = 4,  //!< Kutta's fourth order method, explicit
+    BE    = 5,  //!< Backward Euler, implicit
+    BDF2  = 6   //!< Second order backward differencing, implicit
+  };
+
+
+  //! \brief Returns the temporal order of the given method
+  //! \param[in] method The method
+  //! \returns The order of the given method
+  int Order(Method method);
+
+  //! \brief Returns the number of steps the given method
+  //! \param[in] method The method
+  //! \returns The number of steps in the given method
+  int Steps(Method method);
+}
+
+#endif
