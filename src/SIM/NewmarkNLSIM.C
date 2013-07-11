5fa43ccfbdc6485bd63213814ca3112c99ca3525
kmo <kmo@e10b68d5-8a6e-419e-a041-bce267b0401d>
2013-07-11 15:39:46 +0000

    Added: Nonlinear Newmark simulator (sub-class of NewmarkSIM). Changed NewmarkDriver into a template class such that it can be used both for the linear and nonlinear Newmark simulators.
    git-svn-id: http://svn.sintef.no/trondheim/IFEM/trunk@2464 e10b68d5-8a6e-419e-a041-bce267b0401d

---
+++ src/SIM/NewmarkNLSIM.C
@@ -0,0 +1,126 @@
+// $Id$
+//==============================================================================
+//!
+//! \file NewmarkNLSIM.C
+//!
+//! \date Jul 4 2013
+//!
+//! \author Knut Morten Okstad / SINTEF
+//!
+//! \brief Newmark solution driver for isogeometric dynamic FEM simulators.
+//!
+//==============================================================================
+
+#include "NewmarkNLSIM.h"
+#include "SystemMatrix.h"
+#include "SIMbase.h"
+#include "TimeStep.h"
+#include "Utilities.h"
+#include "tinyxml.h"
+
+
+//! \brief Convenience enum defining total solution vector indices.
+enum { iD=0, iV=2, iA=3, nSOL=4 };
+
+
+NewmarkNLSIM::NewmarkNLSIM (SIMbase& sim) : NewmarkSIM(sim), Finert(NULL)
+{
+  // Default Newmark parameters (alpha = -0.1)
+  beta = 0.3025;
+  gamma = 0.6;
+}
+
+
+bool NewmarkNLSIM::parse (const TiXmlElement* elem)
+{
+  if (strcasecmp(elem->Value(),"newmarksolver"))
+    return model.parse(elem);
+
+  double alpha = -0.1;
+  utl::getAttribute(elem,"alpha",alpha);
+  beta = 0.25*(1.0-alpha)*(1.0-alpha);
+  gamma = 0.5 - alpha;
+
+  utl::getAttribute(elem,"alpha1",alpha1);
+  utl::getAttribute(elem,"alpha2",alpha2);
+
+  const char* value = 0;
+  const TiXmlElement* child = elem->FirstChildElement();
+  for (; child; child = child->NextSiblingElement())
+    if ((value = utl::getValue(child,"maxits")))
+      maxit = atoi(value);
+    else if ((value = utl::getValue(child,"rtol")))
+      convTol = atof(value);
+    else if ((value = utl::getValue(child,"dtol")))
+      divgLim = atof(value);
+
+  return true;
+}
+
+
+void NewmarkNLSIM::init ()
+{
+  model.setIntegrationPrm(0,alpha1);
+  model.setIntegrationPrm(1,alpha2);
+  model.setIntegrationPrm(2,0.5-gamma);
+
+  solution.resize(nSOL);
+  for (Vectors::iterator it = solution.begin(); it != solution.end(); ++it)
+    it->resize(model.getNoDOFs(),true);
+}
+
+
+bool NewmarkNLSIM::advanceStep (TimeStep& param, bool updateTime)
+{
+  // Update displacement solutions between time steps
+  for (int n = solution.size()-3; n > 0; n--)
+    std::copy(solution[n-1].begin(),solution[n-1].end(),solution[n].begin());
+
+  return updateTime ? param.increment() : true;
+}
+
+
+void NewmarkNLSIM::finalizeRHSvector ()
+{
+  if (Finert)
+    model.addToRHSvector(0,*Finert,gamma-0.5);
+}
+
+
+bool NewmarkNLSIM::predictStep (TimeStep& param)
+{
+  if (solution.size() < nSOL) return false;
+
+  Vector v(solution[iV]);
+  Vector a(solution[iA]);
+
+  // Predicted new velocity
+  solution[iV] *= gamma/beta - 1.0;
+  solution[iV].add(a,(0.5*gamma/beta-1.0)*param.time.dt);
+
+  // Predicted new acceleration
+  solution[iA] *= 0.5/beta - 1.0;
+  solution[iA].add(v,1.0/(beta*param.time.dt));
+
+  return true;
+}
+
+
+bool NewmarkNLSIM::correctStep (TimeStep& param, bool converged)
+{
+  if (solution.size() < nSOL) return false;
+
+  // Update current displacement, velocity and acceleration solutions
+  solution[iD].add(linsol,1.0);
+  solution[iV].add(linsol,gamma/(beta*param.time.dt));
+  solution[iA].add(linsol,1.0/(beta*param.time.dt*param.time.dt));
+
+  if (converged)
+  {
+    // Save the actual inertia vector (minus the residual) from converged step
+    delete Finert;
+    Finert = model.getRHSvector(1,true);
+  }
+
+  return model.updateConfiguration(solution[iD]);
+}
---
+++ src/SIM/NewmarkNLSIM.h
@@ -0,0 +1,60 @@
+// $Id$
+//==============================================================================
+//!
+//! \file NewmarkNLSIM.h
+//!
+//! \date Jul 4 2013
+//!
+//! \author Knut Morten Okstad / SINTEF
+//!
+//! \brief Nonlinear solution driver for isogeometric dynamic FEM simulators.
+//!
+//==============================================================================
+
+#ifndef _NEWMARK_NL_SIM_H
+#define _NEWMARK_NL_SIM_H
+
+#include "NewmarkSIM.h"
+
+class SystemVector;
+
+
+/*!
+  \brief Newmark-based solution driver for dynamic isogeometric FEM simulators.
+*/
+
+class NewmarkNLSIM : public NewmarkSIM
+{
+public:
+  //! \brief The constructor initializes default solution parameters.
+  //! \param sim The FE model
+  NewmarkNLSIM(SIMbase& sim);
+  //! \brief Empty destructor.
+  virtual ~NewmarkNLSIM() {}
+
+  //! \brief Parses a data section from an XML document.
+  //! \param[in] elem The XML element to parse
+  virtual bool parse(const TiXmlElement* elem);
+
+  //! \brief Initializes primary solution vectors and integration parameters.
+  virtual void init();
+
+  //! \brief Advances the time step one step forward.
+  //! \param param Time stepping parameters
+  //! \param[in] updateTime If \e false, the time parameters are not incremented
+  virtual bool advanceStep(TimeStep& param, bool updateTime = true);
+
+protected:
+  //! \brief Calculates predicted velocities and accelerations.
+  virtual bool predictStep(TimeStep& param);
+  //! \brief Updates configuration variables (solution vector) in an iteration.
+  virtual bool correctStep(TimeStep& param, bool = false);
+  //! \brief Finalizes the right-hand-side vector on the system level.
+  virtual void finalizeRHSvector();
+
+private:
+  SystemVector* Finert; //!< Actual inertia forces in last converged time step
+
+};
+
+#endif
