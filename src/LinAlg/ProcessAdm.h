f232bfd2e58a76a683275b71698829f2b5066539
rho <rho@e10b68d5-8a6e-419e-a041-bce267b0401d>
2013-10-16 11:19:54 +0000

    Bug fix wrt ProcessAdm
    git-svn-id: http://svn.sintef.no/trondheim/IFEM/trunk@2571 e10b68d5-8a6e-419e-a041-bce267b0401d

--- src/LinAlg/ProcessAdm.h
+++ src/LinAlg/ProcessAdm.h
@@ -59,28 +59,28 @@ class ProcessAdm
   //! \brief Blocking send of an integer
   //! \param[in] value Integer to be sent
   //! \param[in] dest  Process id for destination
-  virtual void send(int value, int dest) const;
+  void send(int value, int dest) const;
   //! \brief Blocking send of an integer vector
   //! \param[in] ivec  Vector to be sent
   //! \param[in] dest  Process id for destination
-  virtual void send(std::vector<int>& ivec, int dest) const;
+  void send(std::vector<int>& ivec, int dest) const;
   //! \brief Blocking send of a double
   //! \param[in] value Double to be sent
   //! \param[in] dest  Process id for destination
-  virtual void send(double value, int dest) const;
+  void send(double value, int dest) const;
   //! \brief Blocking send of a double vector
   //! \param[in] rvec  Vector to be sent
   //! \param[in] dest  Process id for destination
-  virtual void send(std::vector<double>& rvec, int dest) const;
+  void send(std::vector<double>& rvec, int dest) const;
 
   //! \brief Blocking receive of an integer
   //! \param[out] value  Received value
   //! \param[in] source Process id for source
-  virtual void receive(int& value, int source) const;
+  void receive(int& value, int source) const;
   //! \brief Blocking receive of an integer vector
   //! \param[out] ivec   Integer vector to receive
   //! \param[in]  source Process id for source
-  virtual void receive(std::vector<int>& ivec, int source) const;
+  void receive(std::vector<int>& ivec, int source) const;
   //! \brief Blocking receive of a double
   //! \param[out] value  Received value
   //! \param[in]  source Process id for source
@@ -88,24 +88,24 @@ class ProcessAdm
   //! \brief Blocking receive of a double vector
   //! \param[out] rvec   Double vector to receive
   //! \param[in]  source Process id for source
-  virtual void receive(std::vector<double>& rvec, int source) const;
+  void receive(std::vector<double>& rvec, int source) const;
 
   //! \brief AllReduce for integer
   //! \param[in] value Integer to be reduced
   //! \param[in] oper  MPI operator (ex. MPI_MIN, MPI_MAX, MPI_SUM, MPI_PROD, ..)
-  virtual int allReduce(int value, MPI_Op oper) const;
+  int allReduce(int value, MPI_Op oper) const;
   //! \brief AllReduce for integer vector
   //! \param[inout] ivec  Integer array to be reduced
   //! \param[in]    oper  MPI operator (ex. MPI_MIN, MPI_MAX, MPI_SUM, MPI_PROD, ..)
-  virtual void allReduce(std::vector<int>& ivec, MPI_Op oper) const;
+  void allReduce(std::vector<int>& ivec, MPI_Op oper) const;
   //! \brief AllReduce for double
   //! \param[in] value Double to be reduced
   //! \param[in] oper  MPI operator (ex. MPI_MIN, MPI_MAX, MPI_SUM, MPI_PROD, ..)
-  virtual double allReduce(double value, MPI_Op oper) const;
+  double allReduce(double value, MPI_Op oper) const;
   //! \brief AllReduce for double vector
   //! \param[inout] rvec  Double array to be reduced
   //! \param[in]    oper  MPI operator (ex. MPI_MIN, MPI_MAX, MPI_SUM, MPI_PROD, ..)
-  virtual void allReduce(std::vector<double>& ivec, MPI_Op oper) const;
+  void allReduce(std::vector<double>& ivec, MPI_Op oper) const;
 #endif
 };
 
