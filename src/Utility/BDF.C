5905510783b040f57812c235940f7bd7f4006b6f
rho <rho@e10b68d5-8a6e-419e-a041-bce267b0401d>
2013-12-10 15:25:39 +0000

    Moved BDF scheme to src/Utilities
    git-svn-id: http://svn.sintef.no/trondheim/IFEM/trunk@2635 e10b68d5-8a6e-419e-a041-bce267b0401d

--- src/Utility/BDF.C
+++ src/Utility/BDF.C
@@ -63,3 +63,62 @@ double TimeIntegration::BDF::extrapolate (const double* values) const
   else // first order
     return values[0];
 }
+
+
+TimeIntegration::BDFD2::BDFD2 (int order) 
+{
+  step = 0;
+  this->setOrder(order);
+}
+
+
+void TimeIntegration::BDFD2::setOrder (int order)
+{
+  if (order > 0) {
+    // Assume zero time derivative at t = 0
+    coefs1.resize(2);
+    coefs1[0] = 2.0;
+    coefs1[1] = -2.0;
+  }
+
+  if (order == 2) {
+    // Use second order for second step
+    coefs2.resize(3);
+    coefs2[0] = 2.5;
+    coefs2[1] = -8.0;
+    coefs2[2] = 5.5;
+  }
+
+  coefs.resize(order+2,1.0);
+  if (order == 1) {
+    coefs[0]  = 1.0;
+    coefs[1]  = -2.0;
+    coefs[2]  = 1.0;
+  }
+  else if (order == 2) {
+    coefs[0] =  2.0;
+    coefs[1] = -5.0;
+    coefs[2] =  4.0;
+    coefs[3] = -1.0;
+  }
+}
+
+
+const std::vector<double>& TimeIntegration::BDFD2::getCoefs () const
+{
+  if (step < 2)
+    return coefs1;
+  else if (this->getActualOrder() == 2 && step == 2)
+    return coefs2;
+  else
+    return coefs;
+}
+
+
+double TimeIntegration::BDFD2::extrapolate (const double* values) const
+{
+  if (step > 1 && coefs.size() > 3) // second order
+    return 2.0*values[0] - values[1];
+  else // first order
+    return values[0];
+}
---
+++ src/Utility/BDF.h
@@ -0,0 +1,112 @@
+// $Id$
+//==============================================================================
+//!
+//! \file BDF.h
+//!
+//! \date Oct 29 2012
+//!
+//! \author Arne Morten Kvarving / SINTEF
+//!
+//! \brief Helper functions for BDF based time stepping.
+//!
+//==============================================================================
+
+#ifndef _BDF_H_
+#define _BDF_H_
+
+#include <vector>
+#include <iostream>
+
+namespace TimeIntegration //! Utilities for time integration.
+{
+  /*!
+    \brief Helper class for BDF methods.
+    \details Keeps track of coefficients, extrapolation and startup.
+  */
+
+  class BDF
+  {
+  public:
+    //! \brief The constructor initializes the coefficients.
+    //! \param[in] order The order of the BDF scheme
+    BDF(int order = 0);
+    
+    //! \brief Destructor
+    virtual ~BDF() {}
+    
+    //! \brief Sets the order of the scheme.
+    virtual void setOrder(int order);
+
+    //! \brief Returns the order to be used for current time step.
+    virtual int getOrder() const { return step < 2 ? coefs1.size()-1 : coefs.size()-1; }
+    //! \brief Returns the order of the scheme.
+    virtual int getActualOrder() const { return coefs.size()-1; }
+    //! \brief Returns the degree for the time derivative approximation
+    virtual int getDegree() const { return 1; }
+
+    //! \brief Advances the time stepping scheme.
+    virtual void advanceStep() { ++step; }
+    //! \brief Advances the time stepping scheme.
+    virtual void advanceStep(double dt, double dtn);
+
+    //! \brief Returns the BDF coefficients.
+    virtual const std::vector<double>& getCoefs() const;
+
+    //! \brief Indexing operator returning the idx'th coefficient.
+    double operator[](int idx) const { return this->getCoefs()[idx]; }
+
+    //! \brief Extrapolates values.
+    //! \param[in] values The values to extrapolate
+    //! \return The extrapolated value
+    virtual double extrapolate(const double* values) const;
+
+  protected:
+    std::vector<double> coefs;  //!< The BDF coefficients
+    std::vector<double> coefs1; //!< BDF coefficients for first time step
+    int                 step;   //!< Time step counter
+  };
+
+  /*!
+    \brief Helper class for BDF methods for 2nd order problems
+    \details Keeps track of coefficients, extrapolation and startup.
+  */
+  class BDFD2 : public BDF
+  {
+  public:
+    //! \brief The constructor initializes the coefficients.
+    //! \param[in] order The order of the BDF scheme
+    BDFD2(int order = 2);
+
+    //! \brief Destructor
+    virtual ~BDFD2() {}
+    
+    //! \brief Sets the order of the scheme.
+    void setOrder(int order);
+
+    //! \brief Returns the order to be used for current time step.
+    int getOrder() const  { return step < 3 ? 1 : coefs.size()-2; }
+    //! \brief Returns the order of the scheme.
+    int getActualOrder() const { return coefs.size()-2; }
+    //! \brief Returns the degree for the time derivative approximation
+    virtual int getDegree() const { return 2; }
+
+    //! \brief Advances the time stepping scheme.
+    void advanceStep(double dt, double dtn) { ++step; } 
+
+    //! \brief Returns the BDF coefficients.
+    const std::vector<double>& getCoefs() const;
+
+    //! \brief Indexing operator returning the idx'th coefficient.
+    double operator[](int idx) const { return this->getCoefs()[idx]; }
+
+    //! \brief Extrapolates values.
+    //! \param[in] values The values to extrapolate
+    //! \return The extrapolated value
+    double extrapolate(const double* values) const;
+
+  protected:
+    std::vector<double> coefs2; //!< BDF coefficients for second time step
+  };
+}
+
+#endif
