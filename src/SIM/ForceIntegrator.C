eb691c10928aa793d490942793c8637e25e90690
kmo <kmo@e10b68d5-8a6e-419e-a041-bce267b0401d>
2015-01-29 18:26:10 +0000

    Added: Missing profiling that went out the bath tub during earlier refactoring..
    git-svn-id: http://svn.sintef.no/trondheim/IFEM/trunk@3105 e10b68d5-8a6e-419e-a041-bce267b0401d

--- src/SIM/ForceIntegrator.C
+++ src/SIM/ForceIntegrator.C
@@ -17,6 +17,7 @@
 #include "IntegrandBase.h"
 #include "GlbForceVec.h"
 #include "Vec3.h"
+#include "Profiler.h"
 
 
 Vector SIM::getBoundaryForce (const Vectors& solution, SIMbase* model, int code,
@@ -34,6 +35,8 @@ Vector SIM::getBoundaryForce (const Vectors& solution, SIMbase* model, int code,
 Vector SIM::getBoundaryForce (const Vectors& solution, SIMbase* model, int code,
                               const TimeDomain& time, const Vec3* X0)
 {
+  PROFILE1("SIM::getBoundaryForce");
+
   ForceBase* forceInt = model->getBoundaryForceIntegrand(X0);
   if (!forceInt)
   {
@@ -43,15 +46,13 @@ Vector SIM::getBoundaryForce (const Vectors& solution, SIMbase* model, int code,
 
   forceInt->initBuffer(model->getNoElms());
 
-  if (!integrate(solution,model,code,time,forceInt)) {
+  Vector force;
+  if (integrate(solution,model,code,time,forceInt))
+    forceInt->assemble(force); // Assemble into the global force resultant
+  else
     std::cerr <<" *** SIM::getBoundaryForce: Failed to evaluate boundary force"
               << std::endl;
-    return Vector();
-  }
 
-  // Assemble the element force contributions into the global force resultant
-  Vector force;
-  forceInt->assemble(force);
   delete forceInt;
   return force;
 }
@@ -60,24 +61,22 @@ Vector SIM::getBoundaryForce (const Vectors& solution, SIMbase* model, int code,
 bool SIM::getNodalForces (const Vectors& solution, SIMbase* model, int code,
                           const TimeDomain& time, GlbForceVec& force)
 {
+  PROFILE1("SIM::getNodalForces");
+
   force.initialize();
 
+  bool ok = false;
   ForceBase* forceInt = model->getNodalForceIntegrand();
   if (!forceInt)
-  {
     std::cerr <<" *** SIM::getNodalForces: No force integrand."<< std::endl;
-    return false;
-  }
-
-  if (!integrate(solution,model,code,time,forceInt,&force))
-  {
+  else if (!integrate(solution,model,code,time,forceInt,&force))
     std::cerr <<" *** SIM::getNodalForces: Failed to evaluate nodal forces"
               << std::endl;
-    return false;
-  }
+  else
+    ok = force.finalize();
 
   delete forceInt;
-  return force.finalize();
+  return ok;
 }
 
 
@@ -103,7 +102,7 @@ bool SIM::integrate(const Vectors& solution, SIMbase* model, int code,
   bool ok = true;
   size_t prevPatch = 0;
   GlobalIntegral dummy;
-  GlobalIntegral& frc = force?*force:dummy;
+  GlobalIntegral& frc = force ? *force : dummy;
   PropertyVec::const_iterator p;
   for (p = model->begin_prop(); p != model->end_prop() && ok; p++)
     if (abs(p->pindx) == code)
