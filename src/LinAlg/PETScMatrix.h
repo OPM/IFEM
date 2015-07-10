OPM/IFEM

commit 23441736ce0ca0aba8d89ff62183f757f10ee5d9

Added: Another virtual assemble method in SystemMatrix. Added: Option to delay permanent locking of sparsity pattern in initAssembly. The pattern is only temporarily locked during parallel assembly, but may be unlocked again afterwards for single-thread assembly, if required.
git-svn-id: http://svn.sintef.no/trondheim/IFEM/trunk@1793 e10b68d5-8a6e-419e-a041-bce267b0401d

src/LinAlg/PETScMatrix.h
@@ -16,7 +16,8 @@
 #define _PETSC_MATRIX_H
 
 #include "SystemMatrix.h"
-#include "LinSolParams.h"
+
+class LinSolParams;
 
 
 /*!
@@ -87,14 +88,13 @@ public:
   //! \brief Linfinity-norm of vector.
   virtual real Linfnorm() const;
 
-  //! \brief Return PETSc vector.
+  //! \brief Returns the PETSc vector (for assignment).
   Vec& getVector() { return x; }
-
-  //! \brief Return PETSc vector.
+  //! \brief Returns the PETSc vector (for read access).
   const Vec& getVector() const { return x; }
 
 private:
-  Vec x; //!< The actual PETSc vector.
+  Vec x; //!< The actual PETSc vector
 
 #else // dummy implementation when PETSc is not included
   virtual SystemVector* copy() const { return 0; }
@@ -142,6 +142,13 @@ public:
   //! \brief Creates a copy of the system matrix and returns a pointer to it.
   virtual SystemMatrix* copy() const { return new PETScMatrix(*this); }
 
+  //! \brief Initializes the element assembly process.
+  //! \details Must be called once before the element assembly loop.
+  //! The PETSc data structures are initialized and the all symbolic operations
+  //! that are needed before the actual assembly can start are performed.
+  //! \param[in] sam Auxiliary data describing the FE model topology, etc.
+  virtual void initAssembly(const SAM& sam, bool);
+
   //! \brief Initializes the matrix to zero assuming it is properly dimensioned.
   virtual void init();
 
@@ -154,27 +161,20 @@ public:
   //! is completed on each processor and before the linear system is solved.
   bool endAssembly();
 
-  //! \brief Initializes the element assembly process.
-  //! \details Must be called once before the element assembly loop.
-  //! The PETSc data structures are initialized and the all symbolic operations
-  //! that are need before the actual assembly can start are performed.
-  //! \param[in] sam Auxilliary data describing the FE model topology, etc.
-  virtual void initAssembly(const SAM& sam);
-
-  //! \brief Adds an element stiffness matrix into the system stiffness matrix.
-  //! \param[in] eM  The element stiffness matrix
-  //! \param[in] sam Auxilliary data describing the FE model topology,
+  //! \brief Adds an element matrix into the associated system matrix.
+  //! \param[in] eM  The element matrix
+  //! \param[in] sam Auxiliary data describing the FE model topology,
   //!                nodal DOF status and constraint equations
   //! \param[in] e   Identifier for the element that \a eM belongs to
   //! \return \e true on successful assembly, otherwise \e false
   virtual bool assemble(const Matrix& eM, const SAM& sam, int e);
-  //! \brief Adds an element stiffness matrix into the system stiffness matrix.
+  //! \brief Adds an element matrix into the associated system matrix.
   //! \details When multi-point constraints are present, contributions from
-  //! these are also added into the system right-hand-side load vector.
-  //! \param[in] eM  The element stiffness matrix
-  //! \param[in] sam Auxilliary data describing the FE model topology,
+  //! these are also added into the system right-hand-side vector.
+  //! \param[in] eM  The element matrix
+  //! \param[in] sam Auxiliary data describing the FE model topology,
   //!                nodal DOF status and constraint equations
-  //! \param     B   The system right-hand-side load vector
+  //! \param     B   The system right-hand-side vector
   //! \param[in] e   Identifier for the element that \a eM belongs to
   //! \return \e true on successful assembly, otherwise \e false
   virtual bool assemble(const Matrix& eM, const SAM& sam,
@@ -185,17 +185,19 @@ public:
 
   //! \brief Solves the linear system of equations for a given right-hand-side.
   //! \param B Right-hand-side vector on input, solution vector on output
-  virtual bool solve(SystemVector& B, bool newLHSmatrix = true);
+  //! \param[in] newLHS \e true if the left-hand-side matrix has been updated
+  virtual bool solve(SystemVector& B, bool newLHS = true);
 
   //! \brief Solves the linear system of equations for a given right-hand-side.
-  //! \param b Right-hand-side vector
-  //! \param x Solution vector
-  virtual bool solve(const SystemVector& b, SystemVector& x, bool newLHSmatrix = true);
+  //! \param[in] B Right-hand-side vector
+  //! \param[out] x Solution vector
+  //! \param[in] newLHS \e true if the left-hand-side matrix has been updated
+  virtual bool solve(const SystemVector& B, SystemVector& x, bool newLHS = true);
 
   //! \brief Solves the linear system of equations for a given right-hand-side.
-  //! \param b Right-hand-side vector
+  //! \param b Right-hand-side vector, solution vector on output
   //! \param P Preconditioning matrix (if different than system matrix)
-  //! \param newLHS \e true if the left-hand-side matrix has been updated
+  //! \param[in] newLHS \e true if the left-hand-side matrix has been updated
   virtual bool solve(SystemVector& b, SystemMatrix& P, bool newLHS = true);
 
   //! \brief Solves a generalized symmetric-definite eigenproblem.
@@ -216,30 +218,29 @@ public:
   //! \brief Returns the L-infinity norm of the matrix.
   virtual real Linfnorm() const;
 
-  //! \brief Return PETSc matrix.
+  //! \brief Returns the PETSc matrix (for assignment).
   Mat& getMatrix() { return A; }
-
-  //! \brief Return PETSc matrix.
+  //! \brief Returns the PETSc matrix (for read access).
   const Mat& getMatrix() const { return A; }
 
 private:
-  Mat                 A;         //!< Linear system matrix
-  KSP                 ksp;       //!< Linear solver
+  //! \brief Constructs index set needed for element-by-element preconditioner.
+  bool makeElementIS(const SAM& sam);
+
+  //! \brief Constructs the EBE preconditioner of the given matrix.
+  bool makeEBEpreconditioner(const Mat A, Mat* AeI);
+
+  Mat                 A;         //!< The actual PETSc matrix
+  KSP                 ksp;       //!< Linear equation solver
   MatNullSpace        nsp;       //!< Null-space of linear operator
   const LinSolParams& solParams; //!< Linear solver parameters
   IS*                 elmIS;     //!< Element index sets
   PetscInt            ISsize;    //!< Number of index sets/elements
 
-  //! \brief Constructs index set needed for element-by-element preconditioner
-  bool makeElementIS(const SAM& sam);
-
-  //! \brief Constructs the EBE preconditioner of the given matrix
-  bool makeEBEpreconditioner(const Mat A, Mat* AeI);
-
 #else // dummy implementation when PETSc is not included
   virtual SystemMatrix* copy() const { return 0; }
   virtual void init() {}
-  virtual void initAssembly(const SAM&) {}
+  virtual void initAssembly(const SAM&, bool) {}
   virtual bool assemble(const Matrix&, const SAM&, int) { return false; }
   virtual bool assemble(const Matrix&, const SAM&,
 			SystemVector&, int) { return false; }

src/LinAlg/SystemMatrix.C
@@ -100,3 +100,13 @@ SystemMatrix* SystemMatrix::create (Type matrixType, int num_thread_SLU)
 
   return 0;
 }
+
+
+bool SystemMatrix::assemble (const Matrix&, const SAM&,
+			     SystemVector&, const std::vector<int>&)
+{
+  std::cerr <<"SystemMatrix::assemble(const Matrix&,const SAM&,"
+	    <<"SystemVector&,const std::vector<int>&): "
+	    <<"Not implemented for the chosen matrix type."<< std::endl;
+  return false;
+}

src/ASM/AlgEqSystem.C
@@ -21,6 +21,9 @@ bool AlgEqSystem::init (SystemMatrix::Type mtype, const LinSolParams* spar,
 			size_t nmat, size_t nvec, bool withReactions,
 			int num_threads_SLU)
 {
+  // Using the sign of the num_threads_SLU argument to flag this (convenience)
+  bool dontLockSparsityPattern = num_threads_SLU < 0;
+
   size_t i;
   for (i = nmat; i < A.size(); i++)
     if (A[i]._A) delete A[i]._A;
@@ -38,10 +41,10 @@ bool AlgEqSystem::init (SystemMatrix::Type mtype, const LinSolParams* spar,
       if (spar)
 	A[i]._A = SystemMatrix::create(mtype,*spar);
       else
-	A[i]._A = SystemMatrix::create(mtype,num_threads_SLU);
+	A[i]._A = SystemMatrix::create(mtype,abs(num_threads_SLU));
     if (!A[i]._A) return false;
 
-    A[i]._A->initAssembly(sam);
+    A[i]._A->initAssembly(sam,dontLockSparsityPattern);
     A[i]._b = NULL;
   }
 

src/LinAlg/DenseMatrix.C
@@ -141,7 +141,7 @@ size_t DenseMatrix::dim (int idim) const
 }
 
 
-void DenseMatrix::initAssembly (const SAM& sam)
+void DenseMatrix::initAssembly (const SAM& sam, bool)
 {
   myMat.resize(sam.neq,sam.neq,true);
 }
@@ -280,7 +280,7 @@ bool DenseMatrix::assemble (const Matrix& eM, const SAM& sam, int e)
 }
 
 
-bool DenseMatrix::assemble (const Matrix& eK, const SAM& sam,
+bool DenseMatrix::assemble (const Matrix& eM, const SAM& sam,
 			    SystemVector& B, int e)
 {
   if (myMat.rows() != (size_t)sam.neq || myMat.cols() < (size_t)sam.neq)
@@ -291,19 +291,40 @@ bool DenseMatrix::assemble (const Matrix& eK, const SAM& sam,
 
   int ierr = 0;
 #ifdef USE_F77SAM
-  int* work = new int[eK.rows()];
-  addem2_(eK.ptr(), sam.ttcc, sam.mpar,
+  int* work = new int[eM.rows()];
+  addem2_(eM.ptr(), sam.ttcc, sam.mpar,
 	  sam.madof, sam.meqn, sam.mpmnpc, sam.mmnpc, sam.mpmceq, sam.mmceq,
-	  e, eK.rows(), sam.neq, 6, 1, myMat.ptr(), Bptr->ptr(), work, ierr);
+	  e, eM.rows(), sam.neq, 6, 1, myMat.ptr(), Bptr->ptr(), work, ierr);
   delete[] work;
 #else
   std::vector<int> meen;
-  if (sam.getElmEqns(meen,e,eK.rows()))
-    assemDense(eK,myMat,*Bptr,meen,sam.meqn,sam.mpmceq,sam.mmceq,sam.ttcc);
+  if (sam.getElmEqns(meen,e,eM.rows()))
+    assemDense(eM,myMat,*Bptr,meen,sam.meqn,sam.mpmceq,sam.mmceq,sam.ttcc);
   else
     ierr = 1;
 #endif
   return ierr == 0;
+}
+
+
+bool DenseMatrix::assemble (const Matrix& eM, const SAM& sam,
+			    SystemVector& B, const std::vector<int>& meen)
+{
+#ifndef USE_F77SAM
+  if (myMat.rows() != (size_t)sam.neq || myMat.cols() < (size_t)sam.neq)
+    return false;
+
+  if (eM.rows() < meen.size() || eM.cols() < meen.size())
+    return false;
+
+  StdVector* Bptr = dynamic_cast<StdVector*>(&B);
+  if (!Bptr) return false;
+
+  assemDense(eM,myMat,*Bptr,meen,sam.meqn,sam.mpmceq,sam.mmceq,sam.ttcc);
+  return true;
+#else
+  return false;
+#endif
 }
 
 

src/LinAlg/DenseMatrix.h
@@ -65,30 +65,41 @@ public:
 
   //! \brief Initializes the element assembly process.
   //! \details Must be called once before the element assembly loop.
-  //! \param[in] sam Auxilliary data describing the FE model topology, etc.
-  virtual void initAssembly(const SAM& sam);
+  //! \param[in] sam Auxiliary data describing the FE model topology, etc.
+  virtual void initAssembly(const SAM& sam, bool);
 
   //! \brief Initializes the matrix to zero assuming it is properly dimensioned.
   virtual void init();
 
-  //! \brief Adds an element stiffness matrix into the system stiffness matrix.
-  //! \param[in] eM  The element stiffness matrix
-  //! \param[in] sam Auxilliary data describing the FE model topology,
+  //! \brief Adds an element matrix into the associated system matrix.
+  //! \param[in] eM  The element matrix
+  //! \param[in] sam Auxiliary data describing the FE model topology,
   //!                nodal DOF status and constraint equations
   //! \param[in] e   Identifier for the element that \a eM belongs to
   //! \return \e true on successful assembly, otherwise \e false
   virtual bool assemble(const Matrix& eM, const SAM& sam, int e);
-  //! \brief Adds an element stiffness matrix into the system stiffness matrix.
+  //! \brief Adds an element matrix into the associated system matrix.
   //! \details When multi-point constraints are present, contributions from
-  //! these are also added into the system right-hand-side load vector.
-  //! \param[in] eM  The element stiffness matrix
-  //! \param[in] sam Auxilliary data describing the FE model topology,
+  //! these are also added into the system right-hand-side vector.
+  //! \param[in] eM  The element matrix
+  //! \param[in] sam Auxiliary data describing the FE model topology,
   //!                nodal DOF status and constraint equations
-  //! \param     B   The system right-hand-side load vector
+  //! \param     B   The system right-hand-side vector
   //! \param[in] e   Identifier for the element that \a eM belongs to
   //! \return \e true on successful assembly, otherwise \e false
   virtual bool assemble(const Matrix& eM, const SAM& sam,
 			SystemVector& B, int e);
+  //! \brief Adds an element matrix into the associated system matrix.
+  //! \details When multi-point constraints are present, contributions from
+  //! these are also added into the system right-hand-side vector.
+  //! \param[in] eM   The element matrix
+  //! \param[in] sam  Auxiliary data describing the FE model topology,
+  //!                 nodal DOF status and constraint equations
+  //! \param     B    The system right-hand-side vector
+  //! \param[in] meen Matrix of element equation numbers
+  //! \return \e true on successful assembly, otherwise \e false
+  virtual bool assemble(const Matrix& eM, const SAM& sam,
+			SystemVector& B, const std::vector<int>& meen);
 
   //! \brief Augments a similar matrix symmetrically to the current matrix.
   //! \param[in] B  The matrix to be augmented

src/LinAlg/PETScMatrix.C
@@ -13,6 +13,7 @@
 
 #include "PETScMatrix.h"
 #ifdef HAS_PETSC
+#include "LinSolParams.h"
 #include "SAM.h"
 #include "petscversion.h"
 #include "petscis.h"
@@ -63,7 +64,7 @@ PETScVector::PETScVector(const real* values, size_t n)
   VecSetFromOptions(x);
   VecGetArray(x,&x_array);
   *x_array = *values;
-  VecRestoreArray(x,&x_array);    
+  VecRestoreArray(x,&x_array);
   LinAlgInit::increfs();
 }
 
@@ -103,7 +104,7 @@ real* PETScVector::getPtr()
 {
   real* ptr = 0;
 
-  VecGetArray(x,&ptr); 
+  VecGetArray(x,&ptr);
   return ptr;
 }
 
@@ -196,8 +197,6 @@ PETScMatrix::PETScMatrix(const LinSolParams& spar) : solParams(spar)
 
 PETScMatrix::PETScMatrix (const PETScMatrix& B) : solParams(B.solParams)
 {
-  // Duplicate matrix. 
-  //A = MatDuplicate(B.A);
   MatDuplicate(B.A,MAT_COPY_VALUES,&A);
 
   // Create linear solver object.
@@ -218,14 +217,14 @@ PETScMatrix::PETScMatrix (const PETScMatrix& B) : solParams(B.solParams)
 PETScMatrix::~PETScMatrix ()
 {
   // Deallocation of null space
-  if (solParams.getNullSpace() == CONSTANT) 
+  if (solParams.getNullSpace() == CONSTANT)
     MatNullSpaceDestroy(PETSCMANGLE(nsp));
 
   // Deallocation of linear solver object.
   KSPDestroy(PETSCMANGLE(ksp));
 
   // Deallocation of matrix object.
-  MatDestroy(PETSCMANGLE(A));   
+  MatDestroy(PETSCMANGLE(A));
   LinAlgInit::decrefs();
 
   for (int i = 0;i < ISsize;i++)
@@ -255,9 +254,7 @@ static void assemPETSc (const Matrix& eM, Mat SM, PETScVector& SV,
   // Cast to non-constant Matrix to modify for Dirichlet BCs
   Matrix& A = const_cast<Matrix&>(eM);
 
-  std::vector<real> uc, bc;
-  uc.resize(nedof);
-  bc.resize(nedof);
+  std::vector<real> uc(nedof), bc(nedof);
 
   bool rhsMod = false;
   for (j = 1;j <= nedof;j++) {
@@ -285,8 +282,6 @@ static void assemPETSc (const Matrix& eM, Mat SM, PETScVector& SV,
     bc[j-1] = -uc[j-1];
     for (i = 1;i <= nedof;i++)
       A(i,j) = A(j,i) = 0.0;
-    
-    
     A(j,j) = 1.0;
   }
 
@@ -313,8 +308,8 @@ static void assemPETSc (const Matrix& eM, Mat SM, PETScVector& SV,
 
   // Get C array
   PetscScalar* svec;
-  VecGetArray(SV.getVector(),&svec); 
-  
+  VecGetArray(SV.getVector(),&svec);
+
   // Number of degrees of freedom for element
   size_t nedof = meen.size();
 
@@ -323,7 +318,7 @@ static void assemPETSc (const Matrix& eM, Mat SM, PETScVector& SV,
   for (i = 0; i < nedof; i++)
     l2g[i] = meen[i]-1;
 
-  // Cast to non-constant Matrix 
+  // Cast to non-constant Matrix
   Matrix& A = const_cast<Matrix&>(eM);
 
   // Add element stiffness matrix to global matrix
@@ -341,11 +336,11 @@ static void assemPETSc (const Matrix& eM, Mat SM, PETScVector& SV,
     c0 = ttcc[jp-1];
 
     // Add contributions to SV (righthand side)
-    if (SV.dim() > 0) 
+    if (SV.dim() > 0)
       for (i = 1; i <= nedof; i++) {
 	ieq  = meen[i-1];
 	iceq = -ieq;
-	if (ieq > 0) 
+	if (ieq > 0)
 	  svec[ieq-1] -= c0*eM(i,j);
 	else if (iceq > 0)
 	  for (ip = mpmceq[iceq-1]; ip < mpmceq[iceq]-1; ip++)
@@ -364,9 +359,8 @@ static void assemPETSc (const Matrix& eM, Mat SM, PETScVector& SV,
 	  ieq = meen[i-1];
 	  iceq = -ieq;
 	  if (ieq > 0)
-	    if (ieq == jeq) 
+	    if (ieq == jeq)
 	      MatSetValue(SM,ieq-1,jeq-1,(ttcc[jp]+ttcc[jp])*eM(i,j),ADD_VALUES);
-	  
 	    else {
 	      MatSetValue(SM,ieq-1,jeq-1,ttcc[jp]*eM(i,j),ADD_VALUES);
 	      MatSetValue(SM,jeq-1,ieq-1,ttcc[jp]*eM(j,i),ADD_VALUES);
@@ -380,9 +374,8 @@ static void assemPETSc (const Matrix& eM, Mat SM, PETScVector& SV,
 	}
       }
     }
-    
   }
-   
+
   // Restore vector values
   VecRestoreArray(SV.getVector(),&svec);
 
@@ -407,7 +400,7 @@ static void assemPETSc (const Matrix& eM, Mat SM, const std::vector<int>& meen,
   for (i = 0; i < nedof; i++)
     l2g[i] = meen[i]-1;
 
-  // Cast to non-constant Matrix 
+  // Cast to non-constant Matrix
   Matrix& A = const_cast<Matrix&>(eM);
 
   // Add element stiffness matrix to global matrix
@@ -416,7 +409,7 @@ static void assemPETSc (const Matrix& eM, Mat SM, const std::vector<int>& meen,
   A.transpose();
 
   // Add (appropriately weighted) elements corresponding to constrained
-  // (dependent and prescribed) dofs in eM into SM 
+  // (dependent and prescribed) dofs in eM into SM
   for (j = 1; j <= nedof; j++) {
     jceq = -meen[j-1];
     if (jceq < 1) continue;
@@ -444,7 +437,6 @@ static void assemPETSc (const Matrix& eM, Mat SM, const std::vector<int>& meen,
 	}
       }
     }
-    
   }
 
   // Deallocate memory for l2g mapping
@@ -452,7 +444,7 @@ static void assemPETSc (const Matrix& eM, Mat SM, const std::vector<int>& meen,
 }
 
 
-void PETScMatrix::initAssembly (const SAM& sam)
+void PETScMatrix::initAssembly (const SAM& sam, bool)
 {
   // Get number of equations in linear system
   const PetscInt neq = sam.getNoEquations();
@@ -466,7 +458,7 @@ void PETScMatrix::initAssembly (const SAM& sam)
   MPI_Barrier(PETSC_COMM_WORLD);
   MatSetFromOptions(A);
 
-  // Allocation of sparse pattern.
+  // Allocation of sparsity pattern
 #ifdef PARALLEL_PETSC
   PetscInt ifirst, ilast;
   std::vector<int> d_nnz, o_nnz;
@@ -474,13 +466,12 @@ void PETScMatrix::initAssembly (const SAM& sam)
   MatGetOwnershipRange(A,&ifirst,&ilast);
   if (sam.getNoDofCouplings(ifirst,ilast,d_nnz,o_nnz))
   {
-    std::vector<PetscInt> d_Nnz;
-    std::vector<PetscInt> o_Nnz;
-    d_Nnz.resize(d_nnz.size());
-    for (size_t i=0;i<d_nnz.size();++i) 
+    size_t i;
+    std::vector<PetscInt> d_Nnz(d_nnz.size());
+    std::vector<PetscInt> o_Nnz(o_nnz.size());
+    for (i = 0; i < d_nnz.size(); i++)
       d_Nnz[i] = d_nnz[i];
-    o_Nnz.resize(o_nnz.size());
-    for (size_t i=0;i<o_nnz.size();++i) 
+    for (i = 0; i < o_nnz.size(); i++)
       o_Nnz[i] = o_nnz[i];
     MatMPIAIJSetPreallocation(A,PETSC_DEFAULT,&(d_Nnz[0]),PETSC_DEFAULT,&(o_Nnz[0]));
   }
@@ -492,12 +483,10 @@ void PETScMatrix::initAssembly (const SAM& sam)
   std::vector<int> nnz;
 
   if (sam.getNoDofCouplings(nnz)) {
-    std::vector<PetscInt> Nnz;
-    Nnz.resize(nnz.size());
-    for (size_t i=0;i<nnz.size();++i) 
+    std::vector<PetscInt> Nnz(nnz.size());
+    for (size_t i = 0; i < nnz.size(); i++)
       Nnz[i] = nnz[i];
-    PetscInt* nnzPtr = &(Nnz[0]);
-    MatSeqAIJSetPreallocation(A,PETSC_DEFAULT,nnzPtr);
+    MatSeqAIJSetPreallocation(A,PETSC_DEFAULT,&(Nnz[0]));
   }
   else {
     const PetscInt maxdofc = sam.getMaxDofCouplings();
@@ -602,7 +591,7 @@ bool PETScMatrix::solve (SystemVector& B, bool newLHS)
   Vec x;
   VecDuplicate(Bptr->getVector(),&x);
   VecCopy(Bptr->getVector(),x);
-  
+
   // Has lefthand side changed?
   if (!strncasecmp(solParams.getPreconditioner(),"mat",3)) {
     if (!this->makeEBEpreconditioner(A,&AebeI))
@@ -613,7 +602,7 @@ bool PETScMatrix::solve (SystemVector& B, bool newLHS)
     KSPSetOperators(ksp,A,A,SAME_NONZERO_PATTERN);
   else
     KSPSetOperators(ksp,A,A,SAME_PRECONDITIONER);
-  
+
   solParams.setParams(ksp);
   KSPSetInitialGuessKnoll(ksp,PETSC_TRUE);
   KSPSolve(ksp,x,Bptr->getVector());
@@ -645,7 +634,7 @@ bool PETScMatrix::solve (const SystemVector& b, SystemVector& x, bool newLHS)
     KSPSetOperators(ksp,A,A,SAME_NONZERO_PATTERN);
   else
     KSPSetOperators(ksp,A,A,SAME_PRECONDITIONER);
-  
+
   solParams.setParams(ksp);
   KSPSetInitialGuessNonzero(ksp,PETSC_TRUE);
   KSPSolve(ksp,Bptr->getVector(),Xptr->getVector());
@@ -677,7 +666,7 @@ bool PETScMatrix::solve (SystemVector& B, SystemMatrix& P, bool newLHS)
     KSPSetOperators(ksp,A,Pptr->getMatrix(),SAME_NONZERO_PATTERN);
   else
     KSPSetOperators(ksp,A,Pptr->getMatrix(),SAME_PRECONDITIONER);
-  
+
   solParams.setParams(ksp);
   KSPSetInitialGuessKnoll(ksp,PETSC_TRUE);
   KSPSolve(ksp,x,Bptr->getVector());
@@ -704,9 +693,9 @@ bool PETScMatrix::solveEig (PETScMatrix& B, RealArray& val,
   EPSCreate(PETSC_COMM_WORLD,&eps);
 
   MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);
-  MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY); 
+  MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);
   MatAssemblyBegin(B.A,MAT_FINAL_ASSEMBLY);
-  MatAssemblyEnd(B.A,MAT_FINAL_ASSEMBLY); 
+  MatAssemblyEnd(B.A,MAT_FINAL_ASSEMBLY);
 
   EPSSetOperators(eps,A,B.A);
   EPSSetProblemType(eps,EPS_GHEP);
@@ -718,7 +707,7 @@ bool PETScMatrix::solveEig (PETScMatrix& B, RealArray& val,
   EPSSetFromOptions(eps);
   EPSSolve(eps);
   EPSGetConverged(eps,&nconv);
-  
+
   MatGetSize(A,&m,&n);
   if (m != n) return false;
 
@@ -732,13 +721,11 @@ bool PETScMatrix::solveEig (PETScMatrix& B, RealArray& val,
   for (int i = 0; i < nv; i++) {
     EPSGetEigenpair(eps,i,&kr,&ki,xr,xi);
     VecGetArray(xr,&xrarr);
-    
     val[i] = kr;
     vec.fillColumn(i+1,xrarr);
-    
     VecRestoreArray(xr,&xrarr);
   }
-  
+
   VecDestroy(PETSCMANGLE(xi));
   VecDestroy(PETSCMANGLE(xr));
 
@@ -790,7 +777,7 @@ bool PETScMatrix::makeElementIS(const SAM& sam)
     ISCreateGeneral(PETSC_COMM_SELF,ndof,l2g,&(elmIS[e-1]));
 #endif
   }
-   
+
   return true;
 }
 

src/LinAlg/SAM.C
@@ -360,9 +360,9 @@ bool SAM::getDofCouplings (std::vector<IntSet>& dofc) const
 
 
 bool SAM::initForAssembly (SystemMatrix& sysK, SystemVector& sysRHS,
-			   Vector* reactionForces) const
+			   Vector* reactionForces, bool dontLockSP) const
 {
-  sysK.initAssembly(*this);
+  sysK.initAssembly(*this,dontLockSP);
   return this->initForAssembly(sysRHS,reactionForces);
 }
 

src/LinAlg/SAM.h
@@ -81,12 +81,14 @@ public:
   //! \param sysK   The system left-hand-side matrix to be initialized
   //! \param sysRHS The system right-hand-side load vector to be initialized
   //! \param reactionForces Pointer to vector of nodal reaction forces
+  //! \param[in] dontLockSP If \e true, do not lock the matrix sparsity pattern
   //! \return \e false if no free DOFs in the system, otherwise \e true
   //!
   //! \details This method must be called once before the first call to
   //! \a assembleSystem for a given load case or time step.
   bool initForAssembly(SystemMatrix& sysK, SystemVector& sysRHS,
-		       Vector* reactionForces = 0) const;
+		       Vector* reactionForces = 0,
+		       bool dontLockSP = false) const;
 
   //! \brief Initializes a system matrix prior to the element assembly.
   //! \param sysM The system left-hand-side matrix to be initialized

commit 243fc38755e9ac7459661f787ce0c01f58fd9e3b

fixed: compilation with PETSc
broke in r1793

git-svn-id: http://svn.sintef.no/trondheim/IFEM/trunk@1841 e10b68d5-8a6e-419e-a041-bce267b0401d

src/LinAlg/PETScMatrix.h
@@ -17,6 +17,12 @@
 
 #include "SystemMatrix.h"
 
+#ifdef HAS_PETSC
+#include "petscmat.h"
+#include "petscksp.h"
+#include "petscvec.h"
+#endif
+
 class LinSolParams;
 
 

src/LinAlg/SystemMatrix.C
@@ -18,6 +18,7 @@
 #ifdef HAS_PETSC
 #include "PETScMatrix.h"
 #endif
+#include "LinSolParams.h"
 
 
 SystemVector* SystemVector::create (Type vectorType)