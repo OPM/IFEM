OPM/IFEM

commit 92e9836a442f1bc47af7c5e529bb7eda3833bad3

changed: move command line handling into SIMinput
extremely easy to miss a SIM using the old system. now it's
completely transparent.

this fixes parallel chorin simulations (and regression tests)

git-svn-id: http://svn.sintef.no/trondheim/IFEM/trunk@2013 e10b68d5-8a6e-419e-a041-bce267b0401d

src/SIM/NonLinSIM.C
@@ -13,6 +13,7 @@
 
 #include "NonLinSIM.h"
 #include "SIMbase.h"
+#include "IFEM.h"
 #include "IntegrandBase.h"
 #include "TimeStep.h"
 #include "Profiler.h"
@@ -46,6 +47,17 @@ NonLinSIM::NonLinSIM (SIMbase& sim, CNORM n) : model(sim), iteNorm(n)
 
 NonLinSIM::~NonLinSIM ()
 {
+}
+
+
+bool NonLinSIM::read(const char* fileName)
+{
+  model.opt = IFEM_cmdOptions;
+  bool result = SIMinput::read(fileName);
+  for (int i=1; i < IFEM_argc; ++i)
+    model.opt.parseOldOptions(IFEM_argc, IFEM_argv, i);
+
+  return result;
 }
 
 

src/SIM/NonLinSIM.h
@@ -124,6 +124,8 @@ protected:
   virtual bool lineSearch(TimeStep& param);
 
 public:
+  //! \brief Reads model data from the specified input file \a *fileName.
+  virtual bool read(const char* fileName);
   //! \brief Parses a data section from an input stream.
   //! \param[in] keyWord Keyword of current data section to read
   //! \param is The file stream to read from

src/SIM/SIMinput.C
@@ -12,8 +12,11 @@
 //==============================================================================
 
 #include "SIMinput.h"
+#include "IFEM.h"
 #include "Utilities.h"
 #include "tinyxml.h"
+#include "SystemMatrix.h"
+#include "ASMbase.h"
 #ifdef PARALLEL_PETSC
 #include "petscsys.h"
 #endif
@@ -37,10 +40,23 @@ SIMinput::SIMinput ()
 
 bool SIMinput::read (const char* fileName)
 {
+  opt = IFEM_cmdOptions;
+  std::cout << "solver " << opt.solver << std::endl;
+#ifdef HAS_PETSC
+  // In parallel simulations, we need to retain all DOFs in the equation system.
+  // The fixed DOFs (if any) will receive a homogeneous constraint instead.
+  if (opt.solver == SystemMatrix::PETSC) 
+    ASMbase::fixHomogeneousDirichlet = false;
+#endif
+  bool result;
   if (strcasestr(fileName,".xinp"))
-    return this->readXML(fileName);
+    result = this->readXML(fileName);
   else
-    return this->readFlat(fileName);
+    result = this->readFlat(fileName);
+  for (int i=1; i < IFEM_argc; ++i)
+    opt.parseOldOptions(IFEM_argc, IFEM_argv, i);
+
+  return result;
 }
 
 

src/SIM/SIMinput.h
@@ -17,6 +17,8 @@
 #include <iostream>
 #include <vector>
 
+#include "SIMoptions.h"
+
 class TiXmlElement;
 
 
@@ -37,6 +39,7 @@ public:
   //! \brief Reads model data from the specified input file \a *fileName.
   virtual bool read(const char* fileName);
 
+  SIMoptions opt; //!< Simulation control parameters
 private:
   //! \brief Reads a flat text input file.
   bool readFlat(const char* fileName);

src/IFEM.C
@@ -15,9 +15,21 @@
 
 #include <iostream>
 
-bool InitIFEM(int argc, char** argv)
+SIMoptions IFEM_cmdOptions;
+int IFEM_argc;
+char** IFEM_argv;
+
+bool InitIFEM(int argc, char** argv, int myId)
 {
+  IFEM_argc = argc;
+  IFEM_argv = argv;
   LinAlgInit::Init(argc,argv);
+  for (int i=1; i < argc; ++i)
+    IFEM_cmdOptions.parseOldOptions(argc, argv, i);
+
+  if (myId != 0)
+    return true;
+
   std::cout << "\n===== IFEM v" << IFEM_VERSION_MAJOR << "." 
                                 << IFEM_VERSION_MINOR << "." 
                                 << IFEM_VERSION_PATCH << " initialized =====" << std::endl;

src/IFEM.h.in
@@ -17,9 +17,16 @@
 #define IFEM_VERSION_MINOR @IFEM_VERSION_MINOR@ //!< Minor library version
 #define IFEM_VERSION_PATCH @IFEM_VERSION_PATCH@ //!< Patch library version
 
+#include "SIMoptions.h"
+
 //! \brief Initialize IFEM
 //! \param argc The number of command line arguments
 //! \param argv The command line arguments
-bool InitIFEM(int argc, char** argv);
+//! \param myId The MPI process ID
+bool InitIFEM(int argc, char** argv, int myId);
+
+extern SIMoptions IFEM_cmdOptions; //!< Common command line options
+extern int IFEM_argc;
+extern char** IFEM_argv;
 
 #endif

src/SIM/SIMbase.h
@@ -16,7 +16,6 @@
 
 #include "SIMinput.h"
 #include "SIMdependency.h"
-#include "SIMoptions.h"
 #include "TimeDomain.h"
 #include "TopologySet.h"
 #include "Property.h"
@@ -723,8 +722,6 @@ protected:
 public:
   static bool ignoreDirichlet; //!< Set to \e true for free vibration analysis
   static bool preserveNOrder;  //!< Set to \e true to preserve node ordering
-
-  SIMoptions opt; //!< Simulation control parameters
 
 protected:
   //! \brief Scalar field container

commit ec69fe78c657d3b194d3dbebac5bef545de0240a

fixed: adaptive simulations were broken by r2013
the options were not properly pushed down the the real SIM class

git-svn-id: http://svn.sintef.no/trondheim/IFEM/trunk@2113 e10b68d5-8a6e-419e-a041-bce267b0401d

src/SIM/AdaptiveSIM.C
@@ -469,3 +469,9 @@ void AdaptiveSIM::setupProjections()
 {
   projs.resize(model->opt.project.size());
 }
+
+
+void AdaptiveSIM::setOptions(SIMoptions& opt2)
+{
+  model->opt = opt = opt2;
+}

src/SIM/AdaptiveSIM.h
@@ -72,6 +72,9 @@ public:
   //! \brief Initializes the projections.
   void setupProjections();
 
+  //! \brief Wrapper used to handle hierarchy issues
+  virtual void setOptions(SIMoptions& opt2);
+
 protected:
   //! \brief Parses a data section from an input stream.
   //! \param[in] keyWord Keyword of current data section to read

src/SIM/NonLinSIM.C
@@ -523,3 +523,9 @@ void NonLinSIM::dumpResults (double time, std::ostream& os,
   model.dumpResults(solution.front(),time,os,true,precision);
   model.dumpMoreResults(time,os,precision);
 }
+
+
+void NonLinSIM::setOptions(SIMoptions& opt2)
+{ 
+  model.opt = opt = opt2;
+}

src/SIM/NonLinSIM.h
@@ -138,6 +138,8 @@ public:
   //! \brief Returns a list of prioritized XML-tags.
   virtual const char** getPrioritizedTags() const;
 
+  //! \brief Wrapper used to handle hierarchy issues
+  virtual void setOptions(SIMoptions& opt2);
 protected:
   SIMbase& model;    //!< The isogeometric FE model
   Vectors  solution; //!< Primary solution vectors of the last increments

src/SIM/SIMinput.C
@@ -40,7 +40,7 @@ SIMinput::SIMinput ()
 
 bool SIMinput::read (const char* fileName)
 {
-  opt = IFEM::getOptions();
+  setOptions(IFEM::getOptions());
 #ifdef HAS_PETSC
   // In parallel simulations, we need to retain all DOFs in the equation system.
   // The fixed DOFs (if any) will receive a homogeneous constraint instead.

src/SIM/SIMinput.h
@@ -40,6 +40,9 @@ public:
   virtual bool read(const char* fileName);
 
   SIMoptions opt; //!< Simulation control parameters
+
+  //! \brief Wrapper used to handle hierarchy issues
+  virtual void setOptions(SIMoptions& opt2) { opt = opt2; }
 private:
   //! \brief Reads a flat text input file.
   bool readFlat(const char* fileName);