48a901e8a5bf62aa1ec88216d96b3ff3bb482030
Knut Morten Okstad <knut.morten.okstad@sintef.no>
2016-07-11 11:16:48 +0200

    Added: New class for block element matrices. Used for multi-integrand problems with monolithic coupling.

---
+++ src/ASM/BlockElmMats.C
@@ -0,0 +1,201 @@
+// $Id$
+//==============================================================================
+//!
+//! \file BlockElmMats.C
+//!
+//! \date Jul 10 2016
+//!
+//! \author Knut Morten Okstad / SINTEF
+//!
+//! \brief Representation of the block element matrices for a FEM problem.
+//!
+//==============================================================================
+
+#include "BlockElmMats.h"
+
+
+bool BlockElmMats::redim (size_t blkIndex, size_t nen, size_t ncmp, char basis)
+{
+  bool ok = true;
+  if (basis < 0) // Zero diagonal block
+    basis = -basis;
+  else if (blkIndex <= blockInfo.size() && blkIndex < A.size())
+    A[blkIndex].resize(nen*ncmp,nen*ncmp);
+  else
+    ok = false;
+
+  if (blkIndex <= blockInfo.size() && blkIndex < b.size())
+    b[blkIndex].resize(nen*ncmp);
+  else
+    ok = false;
+
+  if (blkIndex == 0)
+    return ok;
+  else if (blkIndex <= blockInfo.size())
+    blockInfo[blkIndex-1] = Block(ncmp,basis);
+  else
+    ok = false;
+
+  if (basis > 0 && (size_t)basis <= basisInfo.size())
+  {
+    basisInfo[basis-1].nen = nen;
+    basisInfo[basis-1].ncmp += ncmp;
+  }
+  else
+    ok = false;
+
+  return ok;
+}
+
+
+bool BlockElmMats::redimOffDiag (size_t blkIndex, char symmetric)
+{
+  size_t nDiagB = blockInfo.size();
+  if (blkIndex <= nDiagB || blkIndex > A.size())
+    return false; // Not an off-diagonal sub-matrix
+
+  size_t kb = nDiagB;
+  bool symm = A.size()-1 < nDiagB*(nDiagB+1)/2;
+  if (symmFlag.empty())
+    symmFlag.resize(nDiagB*(nDiagB-1) / (symm ? 2 : 1), 0);
+  for (size_t ib = 0; ib < nDiagB; ib++)
+    for (size_t jb = symm ? ib+1 : 0; jb < nDiagB; jb++)
+      if (jb != ib && ++kb == blkIndex)
+      {
+        A[kb].resize(basisInfo[blockInfo[ib].basis-1].nen*blockInfo[ib].ncmp,
+                     basisInfo[blockInfo[jb].basis-1].nen*blockInfo[jb].ncmp);
+        kb -= nDiagB;
+        symmFlag[kb-1] = symmetric;
+        return true;
+      }
+
+  return false;
+}
+
+
+bool BlockElmMats::redimNewtonMat ()
+{
+  // Calculate the total number of element dofs
+  size_t neldof = 0;
+  size_t nDiagB = blockInfo.size();
+  for (size_t i = 0; i < nDiagB; i++)
+    neldof += blockInfo[i].ncmp*basisInfo[blockInfo[i].basis-1].nen;
+
+  // Set the element Newton matrix dimension
+  if (!A.empty() && A.front().empty())
+    A.front().resize(neldof,neldof);
+  if (!b.empty() && b.front().empty())
+    b.front().resize(neldof);
+
+  // Calculate the offset of each block sub-matrix
+  size_t idof = 1;
+  for (size_t j = 0; j < basisInfo.size(); j++)
+  {
+    size_t ndof = 0;
+    size_t jdof = idof;
+    for (size_t i = 0; i < nDiagB; i++)
+      if (blockInfo[i].basis == (int)j+1)
+      {
+        blockInfo[i].idof = jdof;
+        jdof += blockInfo[i].ncmp;
+        ndof += blockInfo[i].ncmp*basisInfo[j].nen;
+      }
+    idof += ndof;
+  }
+
+  return A.empty() ? true : idof-1 == A.front().rows();
+}
+
+
+const Matrix& BlockElmMats::getNewtonMatrix () const
+{
+  Matrix& N = const_cast<Matrix&>(A.front());
+
+  size_t ib, jb, kb;
+  size_t nDiagB = blockInfo.size();
+  bool symmetry = A.size()-1 <= nDiagB*(nDiagB+1)/2;
+  for (ib = 0, kb = nDiagB-1; ib < nDiagB; ib++)
+  {
+    size_t ibas = blockInfo[ib].basis-1;
+    size_t neni = basisInfo[ibas].nen;
+    size_t ndi  = basisInfo[ibas].ncmp;
+    size_t nci  = blockInfo[ib].ncmp;
+    size_t idof = blockInfo[ib].idof;
+
+    // Insert the diagonal block matrix
+    const Matrix& Aii = A[1+ib];
+    if (!Aii.empty())
+      for (size_t in = 0; in < neni; in++)
+        for (size_t jn = 0; jn < neni; jn++)
+          for (size_t i = 0; i < nci; i++)
+            for (size_t jd = 0; jd < nci; jd++)
+              N(idof+ndi*in+i,idof+ndi*jn+jd) = Aii(1+nci*in+i,1+nci*jn+jd);
+
+    for (jb = symmetry ? ib+1 : 0; jb < nDiagB && 2+kb < A.size(); jb++)
+    {
+      if (jb == ib)
+        continue;
+      else
+        ++kb;
+
+      size_t jbas = blockInfo[jb].basis-1;
+      size_t nenj = basisInfo[jbas].nen;
+      size_t ndj  = basisInfo[jbas].ncmp;
+      size_t ncj  = blockInfo[jb].ncmp;
+      size_t jdof = blockInfo[jb].idof;
+
+      // Insert the off-diagonal block matrix
+      const Matrix& Aij = A[1+kb];
+      if (!Aij.empty())
+        for (size_t in = 0; in < neni; in++)
+          for (size_t jn = 0; jn < nenj; jn++)
+            for (size_t i = 0; i < nci; i++)
+              for (size_t j = 0; j < ncj; j++)
+              {
+                N(idof+ndi*in+i,jdof+ndj*jn+j) = Aij(1+nci*in+i,1+ncj*jn+j);
+                if (symmFlag[kb-nDiagB] > 0)
+                  N(jdof+ndj*jn+j,idof+ndi*in+i) = Aij(1+nci*in+i,1+ncj*jn+j);
+                else if (symmFlag[kb-nDiagB] < 0)
+                  N(jdof+ndj*jn+j,idof+ndi*in+i) = -Aij(1+nci*in+i,1+ncj*jn+j);
+              }
+    }
+  }
+
+#if SP_DEBUG > 2
+  std::cout << std::endl;
+  for (ib = 1; ib < A.size(); ib++)
+    std::cout <<"Block matrix "<< ib << A[ib];
+  std::cout <<"Resulting Newton matrix"<< A.front();
+#endif
+
+  return A.front();
+}
+
+
+const Vector& BlockElmMats::getRHSVector () const
+{
+  Vector& R = const_cast<Vector&>(b.front());
+
+  for (size_t ib = 0; ib < blockInfo.size(); ib++)
+  {
+    size_t ibas = blockInfo[ib].basis-1;
+    size_t neni = basisInfo[ibas].nen;
+    size_t ndi  = basisInfo[ibas].ncmp;
+    size_t nci  = blockInfo[ib].ncmp;
+    size_t idof = blockInfo[ib].idof;
+
+    const Vector& bi = b[1+ib];
+    for (size_t in = 0; in < neni; in++)
+      for (size_t i = 0; i < nci; i++)
+        R(idof+ndi*in+i) = bi(1+nci*in+i);
+  }
+
+#if SP_DEBUG > 2
+  std::cout << std::endl;
+  for (size_t i = 1; i < b.size(); i++)
+    std::cout <<"Block vector "<< i << b[i];
+  std::cout <<"Resulting right-hand-side vector"<< b.front();
+#endif
+
+  return b.front();
+}
---
+++ src/ASM/BlockElmMats.h
@@ -0,0 +1,92 @@
+// $Id$
+//==============================================================================
+//!
+//! \file BlockElmMats.h
+//!
+//! \date Jul 10 2016
+//!
+//! \author Knut Morten Okstad / SINTEF
+//!
+//! \brief Representation of the block element matrices for a FEM problem.
+//!
+//==============================================================================
+
+#ifndef _BLOCK_ELM_MATS_H
+#define _BLOCK_ELM_MATS_H
+
+#include "ElmMats.h"
+
+
+/*!
+  \brief Class representing the block element matrices for a FEM problem.
+  \details This class can be used for multi-integrand FEM problems with strong
+  coupling, where the element matrices typically consist of blocks resulting
+  from the various integrands. The block sub-matrices are stitched together into
+  an element-level Newton matrix corresponding to all DOFs of the element by the
+  getNewtonMatrix method.
+*/
+
+class BlockElmMats : public ElmMats
+{
+public:
+  //! \brief The constructor initializes the block information arrays.
+  //! \param[in] nBlk Number of matrix blocks (in each direction, row & column)
+  //! \param[in] nb Number of bases (> 1 for mixed problems)
+  BlockElmMats(size_t nBlk, size_t nb = 1) : blockInfo(nBlk), basisInfo(nb) {}
+  //! \brief Empty destructor.
+  virtual ~BlockElmMats() {}
+
+  using ElmMats::redim;
+  //! \brief Sets the dimension of a diagonal block sub-matrix.
+  //! \param[in] blkIndex Sub-matrix block index
+  //! \param[in] nen Number of element nodes for this matrix block
+  //! \param[in] ncmp Number of nodal components for this matrix block
+  //! \param[in] basis Basis index associated with this block. If negative,
+  //! this diagonal block is identically zero, but the dimension parameters are
+  //! used for the associated off-diagonal blocks later.
+  bool redim(size_t blkIndex, size_t nen, size_t ncmp = 1, char basis = 1);
+  //! \brief Sets the dimension of an off-diagonal block sub-matrix.
+  //! \param[in] blkIndex Sub-matrix block index
+  //! \param[in] symmetric Symmetry flag (0: not, 1: yes, -1: skew-symmetric)
+  //!
+  //! \details This method must not be invoked until the \a redim method has
+  //! been invoked for all the diagonal blocks.
+  bool redimOffDiag(size_t blkIndex, char symmetric = 1);
+  //! \brief Sets the dimension of the Newton matrix.
+  //! \details This method must not be invoked until the \a redim method has
+  //! been invoked for all the diagonal blocks.
+  bool redimNewtonMat();
+
+  //! \brief Returns the element-level Newton matrix.
+  virtual const Matrix& getNewtonMatrix() const;
+
+  //! \brief Returns the element-level right-hand-side vector
+  //! associated with the Newton matrix.
+  virtual const Vector& getRHSVector() const;
+
+private:
+  //! \brief A struct with some key parameters for each block.
+  struct Block
+  {
+    char   basis; //!< Basis associated with the block
+    size_t ncmp;  //!< Number of nodal components for the block
+    size_t idof;  //!< The first element DOF of the block
+    //! \brief Default constructor.
+    Block(size_t c = 0, char b = 0) : basis(b), ncmp(c), idof(0) {}
+  };
+
+  //! \brief A struct with some key parameters for each basis.
+  struct Basis
+  {
+    size_t nen;  //!< Number of element nodes for the basis
+    size_t ncmp; //!< Number of nodal components for the basis
+    //! \brief Default constructor.
+    Basis() : nen(0), ncmp(0) {}
+  };
+
+  std::vector<Block> blockInfo; //!< Block information
+  std::vector<Basis> basisInfo; //!< Basis information
+  std::vector<char>  symmFlag;  //!< Symmetry flag for off-diagonal blocks
+};
+
+#endif
