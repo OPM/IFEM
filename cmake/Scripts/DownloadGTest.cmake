62b9da7bcf83b85edb8e2d3cd7a1732594a06469
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2016-08-29 10:43:09 +0200

    second try for URL update
    build server cmake is built without https support

--- cmake/Scripts/DownloadGTest.cmake
+++ cmake/Scripts/DownloadGTest.cmake
@@ -9,7 +9,7 @@ set_directory_properties(PROPERTIES EP_PREFIX ${CMAKE_BINARY_DIR}/third_party)
 # http://stackoverflow.com/questions/9689183/cmake-googletest
 externalproject_add(
     googletest
-    URL https://github.com/google/googletest/archive/release-1.8.0.tar.gz
+    URL http://github.com/google/googletest/archive/release-1.8.0.tar.gz
     # Disable install step
     INSTALL_COMMAND ""
     # Wrap download, configure and build steps in a script to log output
