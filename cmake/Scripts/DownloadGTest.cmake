cd3ba6528c22c7d7dcfa4976ee5edd46815a01f6
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2016-08-29 10:29:31 +0200

    update gtest location
    the old googlecode project was migrated to github.
    update to version 1.8 while at it.

--- cmake/Scripts/DownloadGTest.cmake
+++ cmake/Scripts/DownloadGTest.cmake
@@ -9,7 +9,7 @@ set_directory_properties(PROPERTIES EP_PREFIX ${CMAKE_BINARY_DIR}/third_party)
 # http://stackoverflow.com/questions/9689183/cmake-googletest
 externalproject_add(
     googletest
-    URL http://googletest.googlecode.com/files/gtest-1.7.0.zip
+    URL https://github.com/google/googletest/archive/release-1.8.0.tar.gz
     # Disable install step
     INSTALL_COMMAND ""
     # Wrap download, configure and build steps in a script to log output
@@ -19,11 +19,11 @@ externalproject_add(
 
 # Specify include dir
 externalproject_get_property(googletest source_dir)
-set(GTEST_INCLUDE_DIRS ${source_dir}/include)
+set(GTEST_INCLUDE_DIRS ${source_dir}/googletest/include)
 
 # Library
 externalproject_get_property(googletest binary_dir)
-set(GTEST_LIBRARIES ${binary_dir}/${CMAKE_FIND_LIBRARY_PREFIXES}gtest.a)
+set(GTEST_LIBRARIES ${binary_dir}/googlemock/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest.a)
 add_library(gtest UNKNOWN IMPORTED)
 set_property(TARGET gtest PROPERTY IMPORTED_LOCATION
              ${GTEST_LIBRARIES})
