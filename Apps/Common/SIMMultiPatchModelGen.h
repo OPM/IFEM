1adfcf18048f0113b5686e3428b07616be6d2357
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2016-09-12 11:14:21 +0200

    added: convenience wrapper class for multi-patch generator usage in applications
    note: does not instance for 1D

---
+++ Apps/Common/SIMMultiPatchModelGen.h
@@ -0,0 +1,45 @@
+// $Id$
+//==============================================================================
+//!
+//! \file SIMMultiPatchModelGen.h
+//!
+//! \date Sep 5 2016
+//!
+//! \author Arne Morten Kvarving / SINTEF
+//!
+//! \brief Base class for simulators equipped with multi-patch model generators.
+//!
+//==============================================================================
+
+#ifndef _SIM_MULTI_PATCH_MODEL_GEN_H_
+#define _SIM_MULTI_PATCH_MODEL_GEN_H_
+
+#include "SIMbase.h"
+
+
+/*!
+  \brief Inherit this class to equip your SIM with multi-patch model generators.
+*/
+
+template<class Dim>
+class SIMMultiPatchModelGen : public Dim
+{
+public:
+  //! \brief Constructor for standard problems.
+  //! \param[in] n1 Number of fields
+  SIMMultiPatchModelGen(int n1) : Dim(n1) {}
+
+  //! \brief Constructor for mixed problems.
+  //! \param[in] unf Number of fields on bases
+  SIMMultiPatchModelGen(const SIMbase::CharVec& unf) : Dim(unf) {}
+
+  //! \brief Empty destructor.
+  virtual ~SIMMultiPatchModelGen() {}
+
+protected:
+  //! \brief Instantiate a generator for the finite element model.
+  //! \param[in] geo XML element containing geometry defintion
+  ModelGenerator* createModelGenerator(const TiXmlElement* geo) const override;
+};
+
+#endif
