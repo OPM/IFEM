3cffcdac2a33277036b37d3fce3aeb4c113147cb
Kjetil Andre Johannessen <kjetijo@gmail.com>
2016-11-11 13:13:02 +0100

    Updated petsc requirements

--- cmake/Modules/FindPetsc.cmake
+++ cmake/Modules/FindPetsc.cmake
@@ -2,7 +2,7 @@ find_package(PkgConfig)
 
 set(OLD_PKG $ENV{PKG_CONFIG_PATH})
 set(ENV{PKG_CONFIG_PATH} $ENV{PETSC_DIR}/$ENV{PETSC_ARCH}/lib/pkgconfig)
-pkg_check_modules(PETSC PETSc>=3.4.0)
+pkg_check_modules(PETSC PETSc>=3.6.3)
 set(ENV{PKG_CONFIG_PATH} ${OLD_PKG})
 
 set(PETSC_LIBRARIES ${PETSC_STATIC_LDFLAGS})
