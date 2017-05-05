02d84c07e2dc79d3adeec067a47c5fe8d878f3ce
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2017-05-05 11:49:04 +0200

    fix some issues introduce by virtualizing this function
    you cannot mix virtual and overloads with a default parameter
    making the methods indistinguishable.

--- Apps/Common/SIMCoupled.h
+++ Apps/Common/SIMCoupled.h
@@ -53,7 +53,13 @@ public:
   }
 
   //! \brief Computes the solution for the current time step.
-  virtual bool solveStep(TimeStep& tp, bool firstS1 = true)
+  virtual bool solveStep(TimeStep& tp)
+  {
+    return this->solveStep(tp, true);
+  }
+
+  //! \brief Computes the solution for the current time step.
+  virtual bool solveStep(TimeStep& tp, bool firstS1)
   {
     if (firstS1)
       return S1.solveStep(tp) && S2.solveStep(tp);
