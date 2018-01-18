db3bbe4e51feda4050ce50cb72c7b32488829e99
Knut Morten Okstad <knut.morten.okstad@sintef.no>
2018-01-18 11:42:33 +0100

    Added: Missing doxygen banner, etc.

--- src/Utility/LagrangeInterpolator.h
+++ src/Utility/LagrangeInterpolator.h
@@ -1,22 +1,41 @@
+// $Id$
+//==============================================================================
+//!
+//! \file LagrangeInterpolator.h
+//!
+//! \date Feb 12 2016
+//!
+//! \author Arne Morten Kvarving / SINTEF
+//!
+//! \brief Utilities for Lagrange interpolation.
+//!
+//==============================================================================
+
 #ifndef LAGRANGE_INTERPOLATOR_H_
 #define LAGRANGE_INTERPOLATOR_H_
 
-#include "MatVec.h"
-#include <vector>
+#include "matrix.h"
 
-class LagrangeInterpolator {
-  public:
-    explicit LagrangeInterpolator(const std::vector<double>& grid_) :
-      grid(grid_)
-    {
-    }
 
-    double evaluate(double x, const std::vector<double>& data);
+/*!
+  \brief Lagrange interpolation in one dimension.
+*/
 
-    Matrix get(const std::vector<double>& new_grid);
+class LagrangeInterpolator
+{
+public:
+  //! \brief The constructor initializes the grid point array.
+  explicit LagrangeInterpolator(const std::vector<Real>& g) : grid(g) {}
 
-  protected:
-    std::vector<double> grid; //!< Grid points.
+  //! \brief Evaluate the interpolator at the specified point.
+  Real evaluate(Real x, size_t nr) const;
+  //! \brief Interpolates the given data at the specified point.
+  Real interpolate(Real x, const std::vector<Real>& data) const;
+  //! \brief Returns an interpolation matrix for a grid of data points.
+  utl::matrix<Real> get(const std::vector<Real>& xg) const;
+
+private:
+  std::vector<Real> grid; //!< Grid points
 };
 
 #endif
