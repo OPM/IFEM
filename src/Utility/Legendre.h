30f2f61a5efee45312fefd936a7e65744597b2df
Knut Morten Okstad <knut.morten.okstad@sintef.no>
2018-01-18 11:44:26 +0100

    Changed: Don't use utl::vector where algebra operations are not needed

--- src/Utility/Legendre.h
+++ src/Utility/Legendre.h
@@ -14,7 +14,7 @@
 #ifndef _LEGENDRE_H
 #define _LEGENDRE_H
 
-#include "MatVec.h"
+#include "matrix.h"
 
 
 namespace Legendre
@@ -23,31 +23,31 @@ namespace Legendre
   //! \param[out] weights Computed Gauss-Legendre weight
   //! \param[out] points Computed Gauss-Legendre points
   //! \param[in] n Number of Gauss points
-  bool GL(RealArray& weights, RealArray& points, int n);
+  bool GL(std::vector<Real>& weights, std::vector<Real>& points, int n);
 
   //! \brief Get Gauss-Lobatto-Legendre points and weights in the domain [-1,1].
   //! \param[out] weights Computed Gauss-Legendre weight
   //! \param[out] points Computed Gauss-Legendre points
   //! \param[in] n Number of Gauss points
-  bool GLL(Vector& weights, Vector& points, int n);
+  bool GLL(std::vector<Real>& weights, std::vector<Real>& points, int n);
 
   //! \brief Evaluates the \a n-th Legendre polynomial.
   //! \param[in] n Polynomial degree
   //! \param[in] x Evaluation point
   //! \param[out] retval Polynomial value at point \a x
-  bool LegendreEval(int n, Real x, Real& retval);
+  bool eval(int n, Real x, Real& retval);
 
   //! \brief Evaluates the first derivative of the \a n-th Legendre polynomial.
   //! \param[in] n Polynomial degree
   //! \param[in] x Evaluation point
   //! \param[out] retval Value of the first derivative at point \a x
-  bool LegendreDerEval(int n, Real x, Real& retval);
+  bool derEval(int n, Real x, Real& retval);
 
   //! \brief Evaluates first derivatives of the \a n one-dimensional
   //! Lagrange interpolation polynomials through \a n GLL-points.
-  //! \param[in] n Number of GLL points\polynomials
+  //! \param[in] n Number of GLL points/polynomials
   //! \param[out] der Evaluated values
-  bool basisDerivatives(int n, Matrix& der);
+  bool basisDerivatives(int n, utl::matrix<Real>& der);
 }
 
 #endif
