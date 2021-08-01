e364b22b088b400b7a02fe4f892995c5fd86fabc
Knut Morten Okstad <knut.morten.okstad@sintef.no>
2021-08-01 16:25:02 +0200

    Fixed: A coordinate bug in the ASMuCube constructor. Changed: Use ASMuCube instead of a SIM in the solution transfer unit tests. Added: Unit test for the ASMu3D::DirichletFace struct.

--- src/ASM/LR/Test/TestASMu3D.C
+++ src/ASM/LR/Test/TestASMu3D.C
@@ -26,7 +26,7 @@ class TestASMu3D : public testing::Test,
 
 TEST_P(TestASMu3D, BoundaryNodes)
 {
-  if (GetParam() == 0 || GetParam() > 6)
+  if (GetParam() < 1 || GetParam() > 6)
     return;
 
   SIM3D sim(1);
@@ -40,21 +40,17 @@ TEST_P(TestASMu3D, BoundaryNodes)
   std::vector<int> vec;
   sim.getBoundaryNodes(bcode,vec);
   ASSERT_EQ(vec.size(), 16U);
-  auto it = vec.begin();
-  for (int i = 0; i < 4; ++i)
-    for (int j = 0; j < 4; ++j)
-      if (GetParam() == 1)
-        EXPECT_EQ(*it++, 1+4*(4*i+j));
-      else if (GetParam() == 2)
-        EXPECT_EQ(*it++, 2+4*(4*i+j));
-      else if (GetParam() == 3)
-        EXPECT_EQ(*it++, 16*i+j+1);
-      else if (GetParam() == 4)
-        EXPECT_EQ(*it++, 5+16*i+j);
-      else if (GetParam() == 5)
-        EXPECT_EQ(*it++, 4*i+j+1);
-      else if (GetParam() == 6)
-        EXPECT_EQ(*it++, 17+4*i+j);
+  int i, j, k = 0;
+  for (i = 0; i < 4; ++i)
+    for (j = 0; j < 4; ++j, ++k)
+      switch (GetParam()) {
+      case 1: EXPECT_EQ(vec[k], 1+4*(4*i+j)); break;
+      case 2: EXPECT_EQ(vec[k], 2+4*(4*i+j)); break;
+      case 3: EXPECT_EQ(vec[k], 16*i+j+1); break;
+      case 4: EXPECT_EQ(vec[k], 5+16*i+j); break;
+      case 5: EXPECT_EQ(vec[k], 4*i+j+1); break;
+      case 6: EXPECT_EQ(vec[k], 17+4*i+j); break;
+      }
 }
 
 
@@ -79,8 +75,9 @@ TEST_P(TestASMu3D, ConnectUneven)
   sim.opt.discretization = ASM::LRSpline;
   std::stringstream str;
   str << "src/ASM/Test/refdata/3d_uneven";
-  if (GetParam() > 0)
-    str << "_" << (GetParam()-1)/3 << (GetParam()-1) % 3;
+  int idx = GetParam()-1;
+  if (idx >= 0)
+    str << "_" << idx/3 << idx%3;
   str << ".xinp";
   ASSERT_TRUE(sim.read(str.str().c_str()));
   ASSERT_TRUE(sim.createFEMmodel());
@@ -93,31 +90,42 @@ INSTANTIATE_TEST_CASE_P(TestASMu3D,
                         testing::ValuesIn(tests));
 
 
+class ASMuCube : public ASMu3D
+{
+public:
+  ASMuCube()
+  {
+    std::stringstream geo("700 1 0 0\n3 0\n"
+                          "2 2\n0 0 1 1\n"
+                          "2 2\n0 0 1 1\n"
+                          "2 2\n0 0 1 1\n"
+                          "0 0 0\n1 0 0\n0 1 0\n1 1 0\n"
+                          "0 0 1\n1 0 1\n0 1 1\n1 1 1\n");
+    EXPECT_TRUE(this->read(geo));
+  }
+  virtual ~ASMuCube() {}
+};
+
+
 TEST(TestASMu3D, TransferGaussPtVars)
 {
-  SIM3D sim(1);
-  sim.opt.discretization = ASM::LRSpline;
-
-  ASMu3D* pch = static_cast<ASMu3D*>(sim.createDefaultModel());
-  LR::LRSplineVolume* lr = pch->getVolume();
+  ASMuCube pch;
+  LR::LRSplineVolume* lr = pch.getVolume();
+  ASSERT_TRUE(lr != nullptr);
   lr->generateIDs();
 
   RealArray oldAr(3*3*3), newAr;
   const double* xi = GaussQuadrature::getCoord(3);
   size_t id[3];
   for (size_t idx = 0; idx < 3; ++idx) {
-    SIM3D simNew(1);
-    simNew.opt.discretization = ASM::LRSpline;
-    ASMu3D* pchNew = static_cast<ASMu3D*>(simNew.createDefaultModel());
-    pchNew->uniformRefine(idx, 1);
-    LR::LRSplineVolume* lrNew = pchNew->getVolume();
-    ASSERT_TRUE(lrNew != nullptr);
-    lrNew->generateIDs();
+    ASMuCube pchNew;
+    pchNew.uniformRefine(idx,1);
+    pchNew.getVolume()->generateIDs();
     for (id[2] = 0; id[2] < 3; ++id[2])
       for (id[1] = 0; id[1] < 3; ++id[1])
         for (id[0] = 0; id[0] < 3; ++id[0])
           oldAr[id[0]+(id[1]+id[2]*3)*3] = (1.0 + xi[id[idx]]) / 2.0;
-    pchNew->transferGaussPtVars(lr, oldAr, newAr, 3);
+    pchNew.transferGaussPtVars(lr, oldAr, newAr, 3);
     size_t k = 0;
     for (size_t iEl = 0; iEl < 2; ++iEl)
       for (id[2] = 0; id[2] < 3; ++id[2])
@@ -130,23 +138,18 @@ TEST(TestASMu3D, TransferGaussPtVars)
 
 TEST(TestASMu3D, TransferGaussPtVarsN)
 {
-  SIM3D sim(1), sim2(1);
-  sim.opt.discretization = sim2.opt.discretization = ASM::LRSpline;
-
-  ASMu3D* pch = static_cast<ASMu3D*>(sim.createDefaultModel());
-  LR::LRSplineVolume* lr = pch->getVolume();
+  ASMuCube pch, pchNew;
+  LR::LRSplineVolume* lr = pch.getVolume();
+  ASSERT_TRUE(lr != nullptr);
   lr->generateIDs();
 
-  ASMu3D* pchNew = static_cast<ASMu3D*>(sim2.createDefaultModel());
-  pchNew->uniformRefine(0, 1);
-  LR::LRSplineVolume* lrNew = pchNew->getVolume();
-  ASSERT_TRUE(lrNew != nullptr);
-  lrNew->generateIDs();
+  pchNew.uniformRefine(0,1);
+  pchNew.getVolume()->generateIDs();
 
   RealArray oldAr(3*3*3), newAr;
   std::iota(oldAr.begin(), oldAr.end(), 1);
 
-  pchNew->transferGaussPtVarsN(lr, oldAr, newAr, 3);
+  pchNew.transferGaussPtVarsN(lr, oldAr, newAr, 3);
   static RealArray refAr = {{ 1.0,  1.0,  2.0,
                               4.0,  4.0,  5.0,
                               7.0,  7.0,  8.0,
@@ -165,22 +168,10 @@ TEST(TestASMu3D, TransferGaussPtVarsN)
                              20.0, 21.0, 21.0,
                              23.0, 24.0, 24.0,
                              26.0, 27.0, 27.0}};
-  EXPECT_EQ(refAr.size(), newAr.size());
+  ASSERT_EQ(refAr.size(), newAr.size());
   for (size_t i = 0; i < refAr.size(); ++i)
     EXPECT_FLOAT_EQ(refAr[i], newAr[i]);
 }
-
-
-class ASMuCube : public ASMu3D
-{
-public:
-  ASMuCube()
-  {
-    std::stringstream geo("700 1 0 0\n3 0\n2 2\n0 0 1 1\n2 2\n0 0 1 1\n2 2\n0 0 1 1\n0 0 0\n1 0 0\n0 1 0\n1 1 0\n0 0 1\n1 0 0\n0 1 1\n1 1 1\n");
-    EXPECT_TRUE(this->read(geo));
-  }
-  virtual ~ASMuCube() {}
-};
 
 
 TEST(TestASMu3D, ElementConnectivities)
@@ -206,5 +197,42 @@ TEST(TestASMu3D, ElementConnectivities)
     ASSERT_EQ(neigh[n].size(), ref[n].size());
     for (size_t i = 0; i < neigh[n].size(); ++i)
       EXPECT_EQ(neigh[n][i], ref[n][i]);
+  }
+}
+
+
+class ASMu3DTest : public ASMuCube
+{
+public:
+  void getFaceCorners(std::array<int,4>& corners, int dir)
+  {
+    DirichletFace df(this->getBasis(),dir);
+    for (int i = 0; i < 4; i++)
+      corners[i] = df.corners[i];
+  }
+};
+
+
+TEST(TestASMu3D, DirichletFace)
+{
+  ASMu3DTest pch1;
+  ASMbase::resetNumbering();
+  ASSERT_TRUE(pch1.generateFEMTopology());
+
+  const std::array<std::array<int,4>,7> refArr = {{
+      {{ 1, 2, 3, 4 }}, // Bottom
+      {{ 1, 2, 5, 6 }}, // South
+      {{ 1, 3, 5, 7 }}, // West
+      {{ 0, 0, 0, 0 }}, // None
+      {{ 2, 4, 6, 8 }}, // East
+      {{ 3, 4, 7, 8 }}, // North
+      {{ 5, 6, 7, 8 }}  // Top
+    }};
+
+  std::array<int,4> corners;
+  for (int dir = -3; dir <= 3; dir++)
+  {
+    pch1.getFaceCorners(corners,dir);
+    EXPECT_EQ(corners,refArr[dir+3]);
   }
 }
