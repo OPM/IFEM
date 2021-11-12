107ecfeb5e9172a57e8370e6c9da5403e1e316e0
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2021-11-12 15:23:01 +0100

    added: allow for defining a function for textureproperties
    this allows for a generic f(texture(X)) where f can be scalar or vector
    valued.
    
    ideally f would be a function of property name but
    as we do not have a base class for f: R -> R^3 we have to use the
    f: R^3 -> R^3 type for vector functions. for this reason, both
    scalar and vectorial functions have to refer to the texture value as 'x'.

--- src/Utility/TextureProperties.h
+++ src/Utility/TextureProperties.h
@@ -16,7 +16,9 @@
 
 #include "Function.h"
 #include "MatVec.h"
+
 #include <map>
+#include <memory>
 
 class TiXmlElement;
 class Vec3;
@@ -38,6 +40,12 @@ public:
   //! \param[out] val Property value
   bool getProperty(const std::string& name, const Vec3& X, double& val) const;
 
+  //! \brief Get value for a vector property
+  //! \param[in] name Name of property
+  //! \param[in] X Position (including parameter values) to evaluate property for
+  //! \param[out] val Property value
+  bool getProperty(const std::string& name, const Vec3& X, Vec3& val) const;
+
   //! \brief Check if a property is available.
   //! \param name Name of property
   bool hasProperty(const std::string& name) const;
@@ -45,34 +53,39 @@ public:
 protected:
   //! \brief Struct holding information about a property.
   struct Property {
-    double min; //!< Minimum value
-    double max; //!< Maximum value
+    double min = 0.0; //!< Minimum value
+    double max = 1.0; //!< Maximum value
     Matrix3D textureData; //!< Texture data
     bool prescaled = false; //!< True if data is already scaled
+    std::string func_definition; //!< Non-empty if we have a function of the texture value
+    std::unique_ptr<FunctionBase> function; //!< Function definition for property (if any)
   };
+
+  //! \brief Obtains texture value for a property.
+  double getValue(const Property& prop, const Vec3& X) const;
 
   std::map<std::string, Property> properties; //!< Map of available properties
 };
 
 
 //! \brief Class to use a property as a function.
-class PropertyFunc : public RealFunc {
+template<class Base, class Value>
+class PropertyFuncType : public Base {
 public:
   //! \brief Constructor initializes the members.
   //! \param prop Name of property
   //! \param props Texture property container
-  PropertyFunc(const std::string& prop, const TextureProperties& props)
-     : m_prop(prop), m_props(props)
-   {}
+  PropertyFuncType(const std::string& prop, const TextureProperties& props) :
+    m_prop(prop), m_props(props) {}
 
   //! \brief Empty destructor.
-  virtual ~PropertyFunc() {}
+  virtual ~PropertyFuncType() {}
 
   //! \brief Evaluate function in a point.
   //! \param X Position to evaluate in
-  double evaluate(const Vec3& X) const override
+  Value evaluate(const Vec3& X) const override
   {
-    double val;
+    Value val;
     m_props.getProperty(m_prop, X, val);
     return val;
   }
@@ -81,5 +94,9 @@ protected:
   std::string m_prop; //!< Name of property
   const TextureProperties& m_props; //!< Texture properties container
 };
+
+
+using PropertyFunc = PropertyFuncType<RealFunc,Real>; //!< Convenience type alias for scalars
+using PropertyVecFunc = PropertyFuncType<VecFunc,Vec3>; //!< Convenience type alias for vector
 
 #endif
