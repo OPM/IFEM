9004e7fb85f169a044f182a87d1c79b5224bd511
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2022-05-19 11:16:58 +0200

    changed: remove embedded gtest library
    either install from apt, or rely on the download fallback

--- cmake/Modules/FindTestLib.cmake
+++ cmake/Modules/FindTestLib.cmake
@@ -4,17 +4,7 @@ if(NOT TestLib_FOUND)
     set(TESTLIB_LIBRARY GTest::GTest)
   else()
     find_package(Threads REQUIRED)
-    if(IFEM_INTREE_BUILD OR IFEM_LIBRARY_BUILD OR IFEM_AS_SUBMODULE)
-       add_subdirectory(${IFEM_PATH}/3rdparty/gtest gtest EXCLUDE_FROM_ALL)
-       add_library(GTest::GTest UNKNOWN IMPORTED)
-       set_target_properties(GTest::GTest PROPERTIES
-                             IMPORTED_LOCATION ${PROJECT_BINARY_DIR}/gtest/libgtest.a
-                             INTERFACE_LINK_LIBRARIES Threads::Threads
-                             INTERFACE_INCLUDE_DIRECTORIES ${IFEM_PATH}/3rdparty/gtest/include)
-       add_dependencies(GTest::GTest gtest)
-     else()
-      include(DownloadGTest)
-    endif()
+    include(DownloadGTest)
     get_target_property(TESTLIB_LIBRARY GTest::GTest IMPORTED_LOCATION)
   endif()
 endif()
