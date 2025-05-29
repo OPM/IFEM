284b6b30b9d59b0b72e437cdec1bda8be057c126
Knut Morten Okstad <knut.morten.okstad@sintef.no>
2025-05-29 12:01:10 +0200

    Added: public method SIMsupel::recoverInternalDOFs(), and a protected virtual method with the same name and default implementation using SIMgeneric::recoverInternals(). Changed: Allocate myProblem only if nf is positive.

--- src/SIM/SIMsupel.h
+++ src/SIM/SIMsupel.h
@@ -27,20 +27,32 @@
 
 class SIMsupel : public SIMdummy<SIMgeneric>
 {
+protected:
+  //! \brief Struct with superelement data.
+  struct SuperElm
+  {
+    std::string id;  //!< Superelement id
+    Matrix      MVP; //!< Local-to-global transformation matrix
+    Vector      sol; //!< Recovered primary solution on underlying FE model
+
+    SIMgeneric* sim = nullptr; //!< Pointer to the underlying FE model
+  };
+
 public:
   //! \brief Default constructor.
   //! \param[in] hd Sub-simulator heading
   //! \param[in] nf Dimension of the primary solution field
-  explicit SIMsupel(const char* hd = nullptr, unsigned char nf = 6);
-  //! \brief Empty destructor.
-  virtual ~SIMsupel() {}
+  explicit SIMsupel(const char* hd = nullptr, char nf = 6);
 
-  //! \brief Creates the computational FEM model from the spline patches.
-  //! \param[in] resetNumb If \e 'y', start element and node numbers from zero
+  //! \brief Creates the FE topology of the superelement patches.
   virtual bool createFEMmodel(char resetNumb);
 
   //! \brief Returns the name of this simulator.
   virtual std::string getName() const { return "SIMsupel"; }
+
+  //! \brief Performs recovery of the internal DOFs for superelements.
+  //! \param[in] glbSol Global solution vector
+  bool recoverInternalDOFs(const Vector& glbSol);
 
 protected:
   using SIMdummy<SIMgeneric>::parse;
@@ -55,22 +67,16 @@ protected:
   virtual ASMbase* readPatch(std::istream& isp, int pchInd, const CharVec&,
                              const char* whiteSpace) const;
 
+  //! \brief Recovers the internal DOFs from static condensation.
+  //! \param sup The superelement to recover internal DOFs for
+  //! \param[in] supSol Supernode solution values
+  virtual bool recoverInternalDOFs(const ASMbase*, SuperElm& sup,
+                                   const Vector& supSol) const;
+
 private:
   unsigned char ncmp; //!< Number of primary solution components per node
 
 protected:
-  //! \brief Struct with superelement data.
-  struct SuperElm
-  {
-    std::string id;  //!< Superelement id
-    Matrix      MVP; //!< Local-to-global transformation matrix
-    SIMgeneric* sim; //!< Pointer to underlying FE model of superelement|
-    Vector      sol; //!< Recovered primary solution on underlying FE model
-
-    //! \brief Default constructor.
-    SuperElm() : sim(nullptr) {}
-  };
-
   std::vector<SuperElm> mySups; //!< Superelement data container
 };
 
