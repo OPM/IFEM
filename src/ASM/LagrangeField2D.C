b7296a461a90c39ba2cb1a2185c9bbabd2c0c027
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2025-11-19 15:24:26 +0100

    LagrangeField(s)xD: implement missing functionality, simplify implementation

--- src/ASM/LagrangeField2D.C
+++ src/ASM/LagrangeField2D.C
@@ -17,21 +17,33 @@
 #include "Lagrange.h"
 #include "CoordinateMapping.h"
 
+#include <numeric>
+
 
 LagrangeField2D::LagrangeField2D (const ASMs2DLag* patch,
-                                  const RealArray& v, char, char,
-                                  const char* name) : FieldBase(name)
+                                  const RealArray& v, char, char cmp,
+                                  const char* name)
+  : FieldBase(name), mnpc(patch->getElmNodes(1))
 {
   patch->getNodalCoordinates(coord);
-  patch->getOrder(p1,p2,n2);
-  patch->getSize(n1,n2);
-  nelm = (n1-1)*(n2-1)/(p1*p2);
-  nno = n1*n2;
+  int nf;
+  patch->getOrder(p1,p2,nf);
+
+  nelm = patch->getNoElms();
+  nno = patch->getNoNodes();
 
   // Ensure the values array has compatible length, pad with zeros if necessary
   values.resize(nno);
-  RealArray::const_iterator end = v.size() > nno ? v.begin()+nno : v.end();
-  std::copy(v.begin(),end,values.begin());
+
+  nf = patch->getNoFields(1);
+  size_t ndof = nf > 1 && cmp > 0 ? nf*nno : nno;
+  RealArray::const_iterator vit = v.begin();
+  RealArray::const_iterator end = v.size() > ndof ? vit+ndof : v.end();
+  if (nf == 1 || cmp == 0)
+    std::copy(vit,end,values.begin());
+  else
+    for (size_t i = 0; i < nno && vit != end; ++i, vit += nf)
+      values[i] = *(vit+cmp-1);
 }
 
 
@@ -43,7 +55,7 @@ double LagrangeField2D::valueNode (size_t node) const
 
 double LagrangeField2D::valueFE (const ItgPoint& x) const
 {
-  if (x.iel < 1 || (size_t)x.iel > nelm)
+  if (x.iel < 1 || static_cast<size_t>(x.iel) > nelm)
   {
     std::cerr <<" *** LagrangeField2D::valueFE: Element index "<< x.iel
               <<" out of range [1,"<< nelm <<"]."<< std::endl;
@@ -53,29 +65,18 @@ double LagrangeField2D::valueFE (const ItgPoint& x) const
   Vector N;
   Lagrange::computeBasis(N,p1,x.xi,p2,x.eta);
 
-  div_t divresult = div(x.iel,(n1-1)/p1);
-  int iel1 = divresult.rem;
-  int iel2 = divresult.quot;
-  const int node1 = p1*iel1-1;
-  const int node2 = p2*iel2-1;
-
-  int locNode = 1;
-  double value = 0.0;
-  for (int j = node2; j <= node2+p2; j++)
-    for (int i = node1; i <= node1+p1; i++, locNode++)
-    {
-      int node = (j-1)*n1 + i;
-      value += values(node)*N(locNode);
-    }
-
-  return value;
+  int locNode = 0;
+  return std::accumulate(mnpc[x.iel-1].begin(), mnpc[x.iel-1].end(), 0.0,
+                         [&locNode, &v = values, &N]
+                         (const double acc, const int node)
+                         { return acc + v[node]*N(++locNode); });
 }
 
 
 bool LagrangeField2D::gradFE (const ItgPoint& x, Vector& grad) const
 {
   grad.resize(2,true);
-  if (x.iel < 1 || (size_t)x.iel > nelm)
+  if (x.iel < 1 || static_cast<size_t>(x.iel) > nelm)
   {
     std::cerr <<" *** LagrangeField2D::gradFE: Element index "<< x.iel
               <<" out of range [1,"<< nelm <<"]."<< std::endl;
@@ -87,27 +88,17 @@ bool LagrangeField2D::gradFE (const ItgPoint& x, Vector& grad) const
   if (!Lagrange::computeBasis(Vnod,dNdu,p1,x.xi,p2,x.eta))
     return false;
 
-  div_t divresult = div(x.iel,(n1-1)/p1);
-  int iel1 = divresult.rem;
-  int iel2 = divresult.quot;
-  const int node1 = p1*iel1-1;
-  const int node2 = p2*iel2-1;
+  Matrix Xnod(2,mnpc[x.iel-1].size());
 
-  const int nen = (p1+1)*(p2+1);
-  Matrix Xnod(2,nen);
-
-  int locNode = 1;
-  for (int j = node2; j <= node2+p2; j++)
-    for (int i = node1; i <= node1+p1; i++, locNode++)
-    {
-      int node = (j-1)*n1 + i;
-      Xnod.fillColumn(locNode,coord.getColumn(node));
-      Vnod(locNode) = values(node);
-    }
+  int locNode = 0;
+  for (const int node : mnpc[x.iel-1]) {
+    Xnod.fillColumn(++locNode,coord.getColumn(node+1));
+    Vnod(locNode) = values[node];
+  }
 
   Matrix Jac, dNdX;
   if (!utl::Jacobian(Jac,dNdX,Xnod,dNdu))
     return false; // Singular Jacobian
 
-  return dNdX.multiply(Vnod,grad);
+  return dNdX.multiply(Vnod,grad,true);
 }
--- src/ASM/LagrangeFields3D.h
+++ src/ASM/LagrangeFields3D.h
@@ -58,10 +58,9 @@ public:
   bool gradFE(const ItgPoint& x, Matrix& grad) const;
 
 protected:
+  using IntMat = std::vector<std::vector<int>>; //!< Convenience type
   Matrix coord; //!< Matrix of nodel coordinates
-  int n1; //!< Number of nodes in first parameter direction
-  int n2; //!< Number of nodes in second parameter direction
-  int n3; //!< Number of nodes in third parameter direction
+  IntMat mnpc; //!< Matrix of element nodes
   int p1; //!< Element order in first parameter direction
   int p2; //!< Element order in second parameter direction
   int p3; //!< Element order in third parameter direction
