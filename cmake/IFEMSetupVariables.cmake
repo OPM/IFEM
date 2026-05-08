230c9db54dc704a34107e3bb7547caabaa3726ef
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2026-05-08 10:21:23 +0200

    Apps/Common: rename to Apps/Utils
    rename target to IFEMAppUtils and add alias IFEM::AppUtils

--- cmake/IFEMSetupVariables.cmake
+++ cmake/IFEMSetupVariables.cmake
@@ -1,4 +1,4 @@
 # Setup some variables used across the build system
 
-set(IFEM_REGTEST_PATH ${IFEM_PATH}/Apps/Common/scripts/regtest.sh)
-set(IFEM_IOTEST_PATH ${IFEM_PATH}/Apps/Common/scripts/iotest.sh)
+set(IFEM_REGTEST_PATH ${IFEM_PATH}/Apps/Utils/scripts/regtest.sh)
+set(IFEM_IOTEST_PATH ${IFEM_PATH}/Apps/Utils/scripts/iotest.sh)
